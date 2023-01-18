4ec8daf3818049808b2926b82e9f542f8f1075ce
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-18 15:46:31 +0200

    Add files via upload

---
+++ 03. Basic Syntax, Conditional Statements and Loops - More Exercise/01.SortNumbers.cs
@@ -0,0 +1,52 @@
+﻿//INPUT
+using System.Xml;
+
+double n1 = double.Parse(Console.ReadLine());
+double n2 = double.Parse(Console.ReadLine());
+double n3 = double.Parse(Console.ReadLine());
+
+double biggerNumber = double.MinValue;
+
+//ACTION
+if (n1 > n2 && n1 > n3)
+{
+    Console.WriteLine(n1);
+    if (n2 > n3)
+    {
+        Console.WriteLine(n2);
+        Console.WriteLine(n3);
+    }
+    else
+    {
+        Console.WriteLine(n3);
+        Console.WriteLine(n2);
+    }
+}
+else if (n2 > n1 && n2 > n3)
+{
+    Console.WriteLine(n2);
+    if (n1 > n3)
+    {
+        Console.WriteLine(n1);
+        Console.WriteLine(n3);
+    }
+    else
+    {
+        Console.WriteLine(n3);
+        Console.WriteLine(n1);
+    }
+}
+else if (n3 > n1 && n3 > n2)
+{
+    Console.WriteLine(n3);
+    if (n1 > n2)
+    {
+        Console.WriteLine(n1);
+        Console.WriteLine(n2);
+    }
+    else
+    {
+        Console.WriteLine(n2);
+        Console.WriteLine(n1);
+    }
+}
