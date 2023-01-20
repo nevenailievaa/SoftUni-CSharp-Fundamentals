a457a072246a4fdeaaf7bd5e78d96bb4aa2bc9e2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-20 11:08:31 +0200

    Add files via upload

---
+++ 04. Data Types and Variables - Lab/11.RefactorVolumeOfPyramid.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+Console.Write("Length: "); double length = double.Parse(Console.ReadLine());
+
+Console.Write("Width: "); double width = double.Parse(Console.ReadLine());
+
+Console.Write("Height: "); double height = double.Parse(Console.ReadLine());
+
+//BASE AREA
+double baseArea = length * width;
+
+//VOLUME
+double volume = ((baseArea * height) / 3.0);
+
+//OUTPUT
+Console.WriteLine($"Pyramid Volume: {volume:f2}");
---
+++ 04. Data Types and Variables - Lab/12.RefactorSpecialNumbers.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+int inputNumber = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+int digitsSum = 0;
+
+for (int i = 1; i <= inputNumber; i++)
+{
+    int currentNumber = i;
+
+    while (currentNumber > 0)
+
+    {
+        int lastDigit = currentNumber % 10;
+        currentNumber = (currentNumber - lastDigit) / 10;
+        digitsSum += lastDigit;
+    }
+
+    if (digitsSum == 5 || digitsSum == 7 || digitsSum == 11)
+    {
+        Console.WriteLine($"{i} -> True");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> False");
+    }
+
+    digitsSum = 0;
+}
