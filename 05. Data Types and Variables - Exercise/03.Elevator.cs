0452d85991b09e9fb28c951cf14a536a6c55956a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:56:17 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/03.Elevator.cs
@@ -0,0 +1,17 @@
+﻿//INPUT
+int peopleCount = int.Parse(Console.ReadLine());
+int elevatorCapacity = int.Parse(Console.ReadLine());
+
+//ACTION
+if (elevatorCapacity % peopleCount == 0)
+{
+    Console.WriteLine(elevatorCapacity / peopleCount);
+}
+else if (peopleCount % elevatorCapacity == 0)
+{
+    Console.WriteLine(peopleCount / elevatorCapacity);
+}
+else
+{
+    Console.WriteLine(peopleCount / elevatorCapacity + 1);
+}
---
+++ 05. Data Types and Variables - Exercise/04.SumOfChars.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+int numberCharacters = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+int charsSum = 0;
+
+//ACTION
+for (int i = 1; i <= numberCharacters; i++)
+{
+    char charInput = char.Parse(Console.ReadLine());
+    charsSum += charInput;
+}
+
+//OUTPUT
+Console.WriteLine($"The sum equals: {charsSum}");
