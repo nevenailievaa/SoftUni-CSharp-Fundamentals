d0fe7a5f07df00b4294e4667806ed4950d3bad66
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:56:49 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/05.PrintPartOfASCIITable.cs
@@ -0,0 +1,11 @@
+﻿//INPUT
+int numberOne = int.Parse(Console.ReadLine());
+int numberTwo = int.Parse(Console.ReadLine());
+int currentChar = numberOne;
+
+//OUTPUT
+for (int i = numberOne; i <= numberTwo; i++)
+{
+    Console.Write($"{Convert.ToChar(currentChar)} ");
+    currentChar++;
+}
---
+++ 05. Data Types and Variables - Exercise/06.TriplesOfLatinLetters.cs
@@ -0,0 +1,17 @@
+﻿//INPUT
+int n = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int i = 0; i < n; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+			char firstChar = (char)('a' + i);
+			char secondChar = (char)('a' + j);
+			char thirdChar = (char)('a' + k);
+            Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+		}
+	}
+}
