733708fc0d1f37381d1810b3f8c559d1a894b880
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:57:03 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/07.WaterOverflow.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+short poursCount = short.Parse(Console.ReadLine());
+
+//CHANGEABLE
+short poursSum = 0;
+
+//ACTION
+byte waterTankCapacity = byte.MaxValue;
+
+for (int i = 1; i <= poursCount; i++)
+{
+    short currentPour = short.Parse(Console.ReadLine());
+    poursSum += currentPour;
+
+    if (poursSum > waterTankCapacity)
+    {
+        Console.WriteLine("Insufficient capacity!");
+        poursSum -= currentPour;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(poursSum);
---
+++ 05. Data Types and Variables - Exercise/08.BeerKegs.cs
@@ -0,0 +1,26 @@
+﻿//INPUT
+int beerKegsCount = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+double biggestKegVolume = double.MinValue;
+string biggestKeg = null;
+
+//ACTIONS
+for (int i = 1; i <= beerKegsCount; i++)
+{
+    string kegModel = Console.ReadLine();
+    double kegRadius = double.Parse(Console.ReadLine());
+    int kegHeight = int.Parse(Console.ReadLine());
+
+    //Volume
+    double volumeCurrentKeg = Math.PI * Math.Pow(kegRadius , 2) * kegHeight;
+
+    if (volumeCurrentKeg > biggestKegVolume)
+    {
+        biggestKeg = kegModel;
+        biggestKegVolume = volumeCurrentKeg;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(biggestKeg);
