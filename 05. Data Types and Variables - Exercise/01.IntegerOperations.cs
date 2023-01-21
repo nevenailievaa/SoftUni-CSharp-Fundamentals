6262bf6bc3ac21a353d910c0b1598b45fd6bac57
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:55:57 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/01.IntegerOperations.cs
@@ -0,0 +1,13 @@
+﻿//INPUT
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+int n4 = int.Parse(Console.ReadLine());
+
+//ACTION
+long n1AndN2Sum = (long)(n1 + n2);
+long divideN1AndN2 = (long)(n1AndN2Sum / n3);
+long multiplyTheResult = (long)(divideN1AndN2 * n4);
+
+//OUTPUT
+Console.WriteLine(multiplyTheResult);
---
+++ 05. Data Types and Variables - Exercise/02.SumDigits.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+int inputNumber = int.Parse(Console.ReadLine());
+int inputNumberLength = inputNumber.ToString().Length;
+int digitsSum = 0;
+int currentNumber = inputNumber;
+
+//ACTION
+for (int i = 1; i <= inputNumberLength; i++)
+{
+    int lastDigit = currentNumber % 10;
+    digitsSum += lastDigit;
+    currentNumber = currentNumber / 10;
+}
+
+//OUTPUT
+Console.WriteLine(digitsSum);
