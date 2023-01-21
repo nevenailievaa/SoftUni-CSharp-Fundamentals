5eb3c2e60f908ea332a19ba53c03f80833203036
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:57:25 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/09.SpiceMustFlow.cs
@@ -0,0 +1,24 @@
+﻿//INPUT
+uint firstDayYield = uint.Parse(Console.ReadLine());
+
+//CHANGEABLE
+uint currentDayYield = firstDayYield;
+uint daysCount = 0;
+uint workersConsumationSum = 0;
+
+//ACTION
+while (currentDayYield >= 100)
+{
+    daysCount++;
+    workersConsumationSum += (currentDayYield - 26u);
+    currentDayYield -= 10u;
+}
+
+if (daysCount > 0)
+{
+    workersConsumationSum -= 26u;
+}
+
+//OUTPUT
+Console.WriteLine(daysCount);
+Console.WriteLine(workersConsumationSum);
---
+++ 05. Data Types and Variables - Exercise/10.PokeMon.cs
@@ -0,0 +1,27 @@
+﻿//INPUT
+int pwr = int.Parse(Console.ReadLine()); 
+int trgt = int.Parse(Console.ReadLine());
+int exhaust = int.Parse(Console.ReadLine()); 
+
+//CHANGEABLE
+int pokeCnt = 0;
+int startPwr = pwr;
+
+//ACTION
+while (pwr >= trgt)
+{
+    pokeCnt++;
+    pwr -= trgt;
+
+    if (pwr != 0 && startPwr != 0 && exhaust != 0)
+    {
+        if (startPwr / (double)pwr == 2)
+        {
+            pwr /= exhaust;
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine(pwr);
+Console.WriteLine(pokeCnt);
