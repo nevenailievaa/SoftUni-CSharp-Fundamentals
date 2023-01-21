c5cb855fb0ef08f044f1e69b38aa2ce8a8d19914
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-21 20:57:37 +0200

    Add files via upload

---
+++ 05. Data Types and Variables - Exercise/11.Snowballs.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+//INPUT
+int snowballsCnt = int.Parse(Console.ReadLine());
+
+//CHANGEABLE
+BigInteger maxValue = 0;
+int bestSnow = 0;
+int bestTime = 0;
+int bestQuality = 0;
+
+//ACTION
+for (int i = 1; i <= snowballsCnt; i++)
+{
+    int snow = int.Parse(Console.ReadLine());
+    int time = int.Parse(Console.ReadLine());
+    int quality = int.Parse(Console.ReadLine());
+
+    BigInteger value = BigInteger.Pow(snow / time , quality);
+
+    if (value > maxValue)
+    {
+        maxValue = value;
+        bestSnow = snow;
+        bestTime = time;
+        bestQuality = quality;
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"{bestSnow} : {bestTime} = {maxValue} ({bestQuality})");
