57f126bb9b8ca573bfb076fe58de7497bdb95736
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-24 16:53:52 +0200

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/04.RefactoringPrimeChecker.cs
@@ -0,0 +1,31 @@
+﻿//INPUT
+int inputNumber = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int i = 2; i <= inputNumber; i++)
+{
+    bool isPrime = false;
+
+    for (int j = 2; j <= i; j++)
+    {
+        if (i == j)
+        {
+            isPrime = true;
+        }
+        else if (i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+
+    //OUTPUT
+    if (isPrime)
+    {
+        Console.WriteLine($"{i} -> true");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> false");
+    }
+}
