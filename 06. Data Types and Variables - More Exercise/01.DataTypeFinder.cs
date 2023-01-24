23e6e66853167abfb7be87f2f938c09c931711a9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-24 16:52:13 +0200

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/01.DataTypeFinder.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+string input = Console.ReadLine();
+
+//ACTION
+while (input != "END")
+{
+    if (int.TryParse(input, out int number))
+    {
+        Console.WriteLine($"{input} is integer type");
+    }
+    else if (bool.TryParse(input, out Boolean value))
+    {
+        Console.WriteLine($"{input} is boolean type");
+    }
+    else if (char.TryParse(input, out Char symbol))
+    {
+        Console.WriteLine($"{input} is character type");
+    }
+    else if (double.TryParse(input, out double floatnumber))
+    {
+        Console.WriteLine($"{input} is floating point type");
+    }
+    else
+    {
+        Console.WriteLine($"{input} is string type");
+    }
+
+    input = Console.ReadLine();
+}
