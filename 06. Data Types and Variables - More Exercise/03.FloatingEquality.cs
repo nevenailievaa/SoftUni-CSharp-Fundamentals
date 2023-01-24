f951a48bc7be42bc6bb67659d97d83d9192a51dc
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-24 16:53:24 +0200

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/03.FloatingEquality.cs
@@ -0,0 +1,18 @@
+﻿//INPUT
+double inputNumberOne = double.Parse(Console.ReadLine());
+double inputNumberTwo = double.Parse(Console.ReadLine());
+
+//ACTION
+float numberOneFloat =(float)inputNumberOne;
+float numberTwoFloat =(float)inputNumberTwo;
+
+if (numberOneFloat == numberTwoFloat)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+
+// 75/100 Judge
