3c52296e8bd66668e5ab517d5e96e1ab6910a165
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 00:12:48 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/06.EvenAndOddSubtraction.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+int[] arrayNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//CHANGEABLE
+int sumEvenNumbers = 0;
+int sumOddNumbers = 0;
+
+//ACTION
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+    if (arrayNumbers[i] % 2 == 0)
+    {
+        sumEvenNumbers += arrayNumbers[i];
+    }
+    else
+    {
+        sumOddNumbers += arrayNumbers[i];
+    }
+}
+
+//OUTPUT
+Console.WriteLine(sumEvenNumbers - sumOddNumbers);
