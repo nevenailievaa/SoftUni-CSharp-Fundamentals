d5b67fbe52773b4488e1b82bfc907a7b7a51caad
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 00:12:23 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/04.ReverseArrayOfStrings.cs
@@ -0,0 +1,11 @@
+﻿//INPUT
+string[] digits = Console.ReadLine().Split();
+
+//ACTION
+string[] digitsReversed = digits.Reverse().ToArray();
+
+//OUTPUT
+for (int i = 0; i < digits.Length; i++)
+{
+    Console.Write($"{digitsReversed[i]} ");
+}
