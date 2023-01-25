d7b7f2cef671e4fc686e3fa9128408b7b391975f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 00:12:37 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/05.SumEvenNumbers.cs
@@ -0,0 +1,17 @@
+﻿//INPUT
+int[] arrayNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//CHANGEABLE
+int sumEvenNumbers = 0;
+
+//ACTION
+for (int i = 0; i < arrayNumbers.Length; i++)
+{
+	if (arrayNumbers[i] % 2 == 0)
+	{
+		sumEvenNumbers += arrayNumbers[i];
+	}
+}
+
+//OUTPUT
+Console.WriteLine(sumEvenNumbers);
