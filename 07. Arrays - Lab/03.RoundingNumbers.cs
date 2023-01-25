a9355b723ef77f1fc0b24fcf54e1e30ab6617c4f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 00:12:09 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/03.RoundingNumbers.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+double[] inputNumbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+//ACTION
+for (int i = 0; i < inputNumbers.Length; i++)
+{
+	if (inputNumbers[i] == 0)
+	{
+        Console.WriteLine($"{inputNumbers[i]} => 0");
+    }
+	else
+	{
+        Console.WriteLine($"{inputNumbers[i]} => {Math.Round(inputNumbers[i], MidpointRounding.AwayFromZero)}"); 
+    }
+}
