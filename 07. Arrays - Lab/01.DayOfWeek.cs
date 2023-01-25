02d666fae1ff15d8dcced4dffaf4fd23a82cda2d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-25 23:25:16 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/01.DayOfWeek.cs
@@ -0,0 +1,25 @@
+﻿//INPUT
+int dayNumber = int.Parse(Console.ReadLine());
+
+//ACTION
+string[] daysOfTheWeek =
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+dayNumber--;
+
+if (dayNumber >= 0 && dayNumber <= 6)
+{
+    Console.WriteLine(daysOfTheWeek[dayNumber]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
