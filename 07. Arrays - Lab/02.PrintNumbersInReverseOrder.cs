eb2a2803b0c191bae72086f962257eb195dbd1b7
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-25 23:25:31 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/02.PrintNumbersInReverseOrder.cs
@@ -0,0 +1,13 @@
+﻿//INPUT
+int[] numbers = new int[int.Parse(Console.ReadLine())];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = int.Parse(Console.ReadLine());
+}
+
+//OUTPUT
+for (int j = numbers.Length - 1; j >= 0; j--)
+{
+    Console.Write($"{numbers[j]} ");
+}
