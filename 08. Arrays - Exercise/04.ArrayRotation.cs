c7a30f656be80c042aa433565abd63c0cfc4098a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-27 00:20:48 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/04.ArrayRotation.cs
@@ -0,0 +1,20 @@
+﻿//INPUT
+int[] inputNumbers = (Console.ReadLine()).Split().Select(int.Parse).ToArray();
+int rotations = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int j = 0; j < rotations; j++)
+{
+    int firstElement = inputNumbers[0];
+
+    for (int i = 0; i < inputNumbers.Length-1; i++)
+    {
+        inputNumbers[i] = inputNumbers[i+1];
+    }
+
+    inputNumbers[inputNumbers.Length-1] = firstElement;
+}
+
+//OUTPUT
+Console.WriteLine(string.Join(" ", inputNumbers));
+
