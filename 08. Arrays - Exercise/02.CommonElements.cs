ac38359ea3f6da8d1538aa10118a4c9b02958fc2
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-27 00:20:15 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/02.CommonElements.cs
@@ -0,0 +1,14 @@
+﻿//INPUT
+string[] arrayOne = Console.ReadLine().Split();
+string[] arrayTwo = Console.ReadLine().Split();
+
+for (int i = 0; i < arrayTwo.Length; i++)
+{
+	for (int j = 0; j < arrayOne.Length; j++)
+	{
+		if (arrayTwo[i] == arrayOne[j])
+		{
+			Console.Write(arrayTwo[i] + " ");
+		}
+	}
+}
