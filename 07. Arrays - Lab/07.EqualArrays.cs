61fcbd4def473f802ce562cc2781d2ed6fc3a569
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 22:04:14 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/07.EqualArrays.cs
@@ -0,0 +1,32 @@
+﻿//INPUT
+int[] firstArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int[] secondArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//CHANGEABLE
+int notIdenticalIndex = -1;
+int indexesSum = 0;
+
+//ACTION
+for (int i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i] != secondArray[i])
+    {
+        notIdenticalIndex = i;
+        break;
+    }
+    else
+    {
+        indexesSum += firstArray[i];
+    }
+
+}
+
+//ACTION
+if (notIdenticalIndex == -1)
+{
+    Console.WriteLine($"Arrays are identical. Sum: {indexesSum}");
+}
+else
+{
+    Console.WriteLine($"Arrays are not identical. Found difference at {notIdenticalIndex} index");
+}
