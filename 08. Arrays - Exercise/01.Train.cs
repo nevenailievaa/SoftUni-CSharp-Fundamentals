3f529226ae104263665b7c7acfd26402531e61f5
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-27 00:20:02 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/01.Train.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+int numberWagons = int.Parse(Console.ReadLine());
+
+//CHANGEABLE DATA
+int allPassengersSum = 0;
+
+//ACTION
+int[] peopleOnTheWagon = new int[numberWagons];
+
+for (int i = 0; i < numberWagons; i++)
+{
+    peopleOnTheWagon[i] = int.Parse(Console.ReadLine());
+    allPassengersSum += peopleOnTheWagon[i];
+}
+
+//OUTPUT
+for (int i = 0; i < numberWagons; i++)
+{
+    Console.Write($"{peopleOnTheWagon[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine(allPassengersSum);
