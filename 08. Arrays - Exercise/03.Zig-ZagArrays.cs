036f12979abe02e2c7086e036b89ca57f5bc36c9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-27 00:20:35 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/03.Zig-ZagArrays.cs
@@ -0,0 +1,24 @@
+﻿//INPUT
+int n = int.Parse(Console.ReadLine());
+
+int[] firstArray = new int[n];
+int[] secondArray = new int[n];
+
+//ACTION
+for (int i = 0; i < n; i++)
+{
+    int[] currentInput = (Console.ReadLine()).Split().Select(int.Parse).ToArray();
+	if (i % 2 == 1)
+	{
+		firstArray[i] = currentInput[0];
+		secondArray[i] = currentInput[1];
+	}
+	else
+	{
+        secondArray[i] = currentInput[0];
+        firstArray[i] = currentInput[1];
+    }
+}
+//OUTLINE
+Console.WriteLine(string.Join(" ", secondArray));
+Console.WriteLine(string.Join(" ", firstArray));
