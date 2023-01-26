b93d78d230139945b68d43b6b9fb54da78463c43
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-26 22:04:28 +0200

    Add files via upload

---
+++ 07. Arrays - Lab/08.CondenseArrayToNumber.cs
@@ -0,0 +1,14 @@
+﻿//INPUT
+int[] inputNumbers = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 1 2 3
+
+//ACTION
+for (int i = 0; i < inputNumbers.Length - 1; i++)
+{
+	for (int j = 0; j < inputNumbers.Length -1 - i; j++)
+	{
+        inputNumbers[j] = inputNumbers[j] + inputNumbers[j+1];
+	}
+}
+//OUTPUT
+Console.WriteLine(inputNumbers[0]);
+
