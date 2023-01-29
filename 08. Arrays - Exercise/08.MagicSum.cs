c00dc6ce4d303245968f9b2b72e245c6cb9a33d3
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-29 21:12:51 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/08.MagicSum.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+int[] inputArray = (Console.ReadLine()).Split().Select(int.Parse).ToArray();
+int givenNumber = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int i = 0; i < inputArray.Length; i++)
+{
+	for (int j = i + 1; j < inputArray.Length; j++)
+	{
+		if (inputArray[i] + inputArray[j] == givenNumber)
+		{
+			//OUTPUT
+			Console.WriteLine($"{inputArray[i]} {inputArray[j]}");
+		}
+	}
+}
