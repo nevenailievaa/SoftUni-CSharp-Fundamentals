26c8c493e17566ed8372294b69ca57227443f673
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-29 18:24:22 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/06.EqualSum.cs
@@ -0,0 +1,34 @@
+﻿//INPUT
+int[] inputArray = (Console.ReadLine()).Split().Select(int.Parse).ToArray();
+
+//CHANGEABLE
+int leftSideSum = 0;
+int rightSideSum = 0;
+
+//ACTION
+for (int i = 0; i < inputArray.Length; i++)
+{
+	//Sides sum
+	for (int r = i; r < inputArray.Length; r++)
+	{
+		rightSideSum += inputArray[r];
+	}
+	for (int l = i; l >= 0; l--)
+	{
+		leftSideSum += inputArray[l];
+	}
+
+	//Comparing the sides
+    if (leftSideSum == rightSideSum)
+    {
+        Console.WriteLine(i);
+		return;
+    }
+	else
+	{
+		rightSideSum = 0;
+		leftSideSum = 0;
+	}
+}
+
+Console.WriteLine("no");
