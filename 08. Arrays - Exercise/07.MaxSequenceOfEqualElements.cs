b38129a874682eeca516388dc6bcf5a7f2f48b0c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-01-29 21:12:39 +0200

    Add files via upload

---
+++ 08. Arrays - Exercise/07.MaxSequenceOfEqualElements.cs
@@ -0,0 +1,39 @@
+﻿//INPUT
+int[] inputArray = (Console.ReadLine()).Split().Select(int.Parse).ToArray();
+
+//CHANGEABLE
+int currentEqualElementsCount = 0;
+int equalElementsCount = 0;
+int currentIndexElement = 0;
+
+//ACTION
+for (int i = 0; i < inputArray.Length; i++)
+{
+	//Restore
+    currentEqualElementsCount = 0;
+
+	//Checker
+    for (int j = i; j < inputArray.Length; j++)
+	{
+		if (inputArray[i] == inputArray[j])
+		{
+            currentEqualElementsCount++;
+
+			if (currentEqualElementsCount > equalElementsCount)
+			{
+				equalElementsCount = currentEqualElementsCount;
+				currentIndexElement = inputArray[i];
+            }
+        }
+        else
+		{
+            break;
+		}
+	}
+}
+
+//OUTPUT
+for (int i = 1; i <= equalElementsCount; i++)
+{
+    Console.Write($"{currentIndexElement} ");
+}
