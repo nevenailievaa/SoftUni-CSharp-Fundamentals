cf10d222bea5560512d15b8e4748f6f1d4fbcbd8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-01 14:09:18 +0200

    Update 01.EncryptSortAndPrintArray.cs

--- 09. Arrays - More Exercise/01.EncryptSortAndPrintArray.cs
+++ 09. Arrays - More Exercise/01.EncryptSortAndPrintArray.cs
@@ -1,4 +1,10 @@
-﻿//INPUT
+//Write a program that reads a sequence of strings from the console. Encrypt every string by summing:
+//The code of each vowel multiplied by the string length
+//The code of each consonant divided by the string length
+//Sort the number sequence in ascending order and print it in the console.
+//On the first line, you will always receive the number of strings you have to read.
+
+//INPUT
 int n = int.Parse(Console.ReadLine());
 
 //CHANGEABLE
