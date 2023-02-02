7d129d3988889e7d6fedbf2b1191364a042b096b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-02 16:05:42 +0200

    Add files via upload

---
+++ 11. Methods - Lab/06.CalculateRectangleArea.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Grades
+{
+    static void Main()
+    {
+        //INPUT
+        int inputNumberOne = int.Parse(Console.ReadLine());
+        int inputNumberTwo = int.Parse(Console.ReadLine());
+
+        //OUTPUT
+        AreaCalculation(inputNumberOne, inputNumberTwo);
+    }
+
+    //ACTION
+    static void AreaCalculation(int n1, int n2)
+    {
+        Console.WriteLine(n1 * n2);
+    }
+}
