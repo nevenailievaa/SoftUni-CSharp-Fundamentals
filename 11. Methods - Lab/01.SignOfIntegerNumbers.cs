80cfe134efd6d51882a91a5165814e2d1b084970
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-02 20:46:44 +0200

    Add files via upload

---
+++ 11. Methods - Lab/01.SignOfIntegerNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int n = int.Parse(Console.ReadLine());
+        //OUTPUT
+        PrintNumberSign(n);
+    }
+    //ACTION
+    static void PrintNumberSign(int number)
+    {
+        if (number > 0)
+        {
+            Console.WriteLine($"The number {number} is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"The number {number} is negative.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {number} is zero.");
+        }
+    }
+}
