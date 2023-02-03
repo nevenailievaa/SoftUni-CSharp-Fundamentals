3c5437972eb078479a67d84dc64b881242b05517
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-03 14:32:09 +0200

    Add files via upload

---
+++ 11. Methods - Lab/11.MathOperations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        double firstNumber = double.Parse(Console.ReadLine());
+        string calculator = Console.ReadLine();
+        double secondNumber = double.Parse(Console.ReadLine());
+
+        //ACTION
+        if (calculator == "+")
+        {
+            Console.WriteLine(Addition(firstNumber, secondNumber));
+        }
+        else if (calculator == "*")
+        {
+            Console.WriteLine(Multiplication(firstNumber, secondNumber));
+        }
+        else if(calculator == "-")
+        {
+            Console.WriteLine(Subtraction(firstNumber, secondNumber));
+        }
+        else if(calculator == "/")
+        {
+            Console.WriteLine(Division(firstNumber, secondNumber));
+        }
+    }
+
+    //ACTION
+    static double Addition(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+    static double Subtraction(double n1, double n2)
+    {
+        return n1 - n2;
+    }
+    static double Multiplication(double n1, double n2)
+    {
+        return n1 * n2;
+    }
+    static double Division(double n1, double n2)
+    {
+        return n1 / n2;
+    }
+}
