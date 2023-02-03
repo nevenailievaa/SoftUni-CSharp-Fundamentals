e697d09ef34dcdd81bacee718e152d9d38dbb8a1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-03 15:15:28 +0200

    Add files via upload

---
+++ 12. Methods - Exercise/02.VowelsCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        string input = Console.ReadLine();
+
+        //OUTPUT
+        Console.WriteLine(VowelsCount(input));
+    }
+
+    //ACTION
+    static int VowelsCount(string input)
+    {
+        char[] inputArray = input.ToCharArray();
+        int vowelsCount = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (inputArray[i] == 'A' || inputArray[i] == 'E' || inputArray[i] == 'I'
+                || inputArray[i] == 'O' || inputArray[i] == 'U' || inputArray[i] == 'a'
+                || inputArray[i] == 'e' || inputArray[i] == 'i' || inputArray[i] == 'o'
+                || inputArray[i] == 'u')
+            {
+                vowelsCount++;
+            }
+        }
+        return vowelsCount;
+    }
+}
