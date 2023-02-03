740735f719d958b76b06ca0f2b1a28f5988dae61
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-03 14:31:36 +0200

    Add files via upload

---
+++ 11. Methods - Lab/08.MathPower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int baseNumber = int.Parse(Console.ReadLine());
+        int powerNumber = int.Parse(Console.ReadLine());
+
+        //OUTPUT
+        MathPower(baseNumber, powerNumber);
+    }
+
+    //ACTION
+    static void MathPower(int baseNum, int powerNum)
+    {
+        Console.WriteLine(Math.Pow(baseNum, powerNum));
+    }
+}
