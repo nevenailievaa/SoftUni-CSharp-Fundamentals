b07cce06b88142211254ddf6ab4167ab4f1bb1b9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-03 14:31:21 +0200

    Add files via upload

---
+++ 11. Methods - Lab/07.RepeatString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        string inputString = Console.ReadLine();
+        int repeatCount = int.Parse(Console.ReadLine());
+
+        //OUTPUT
+        Repeat(inputString, repeatCount);
+    }
+
+    //ACTION
+    static void Repeat(string inputString, int repeatCount)
+    {
+        for (int i = 1; i <= repeatCount; i++)
+        {
+            Console.Write(inputString);
+        }
+    }
+}
