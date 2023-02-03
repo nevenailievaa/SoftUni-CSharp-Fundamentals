1f4199559a4b2ba120c47a77ea3820f964eed4ce
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-03 15:14:54 +0200

    Add files via upload

---
+++ 12. Methods - Exercise/01.SmallestOfThreeNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        //OUTPUT
+        Console.WriteLine(SmallestNumber(firstNumber, secondNumber, thirdNumber));
+    }
+
+    //ACTION
+    static int SmallestNumber(int n1, int n2, int n3)
+    {
+        int smallestNumber = int.MaxValue;
+
+        if (n1 < smallestNumber)
+        {
+            smallestNumber = n1;
+        }
+        if (n2 < smallestNumber)
+        {
+            smallestNumber = n2;
+        }
+        if (n3 < smallestNumber)
+        {
+            smallestNumber = n3;
+        }
+
+        return smallestNumber;
+    }
+}
