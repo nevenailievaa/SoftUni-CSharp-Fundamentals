659816b80ea4c52e66bc661b9ebfb99751979e84
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-04 12:54:54 +0200

    Add files via upload

---
+++ 12. Methods - Exercise/06.MiddleCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        string input = Console.ReadLine();
+
+        //OUTPUT
+        MiddleCharacters(input);
+    }
+
+    //ACTION
+    static void MiddleCharacters(string input)
+    {
+        char[] chars = input.ToCharArray();
+
+        char firstMiddleChar;
+        char secondMiddleChar;
+
+        if (input.Length % 2 == 0)
+        {
+            firstMiddleChar = chars[input.Length/2 - 1];
+            secondMiddleChar = chars[input.Length/2];
+            Console.WriteLine($"{firstMiddleChar}{secondMiddleChar}");
+        }
+        else
+        {
+            firstMiddleChar = chars[input.Length/2];
+            Console.WriteLine(firstMiddleChar);
+        }
+    }
+}
