888ef48fac28a6acbedf1edfcd799762acb07362
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-04 12:54:45 +0200

    Add files via upload

---
+++ 12. Methods - Exercise/05.AddAndSubtract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        //ACTION
+        int d = Sum(a, b);
+        int result = Subtract(d, c);
+
+        //OUTPUT
+        Console.WriteLine(result);
+    }
+
+    //ACTION
+    static int Sum(int a, int b)
+    {
+        return a+b;
+    }
+    static int Subtract(int d, int c)
+    {
+        return d - c;
+    }
+}
