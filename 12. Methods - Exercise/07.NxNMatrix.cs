8a9e3d8c57bb5f875f3904c63e56496433e5950b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-04 12:55:04 +0200

    Add files via upload

---
+++ 12. Methods - Exercise/07.NxNMatrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int inputInteger = int.Parse(Console.ReadLine());
+
+        //OUTPUT
+        Matrix(inputInteger);
+    }
+
+    //ACTION
+    static void Matrix(int number)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            for (int j = 1; j <= number; j++)
+            {
+                Console.Write(number + " ");
+            }
+            //New Line
+            Console.WriteLine();
+        }
+    }
+}
