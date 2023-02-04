4edf63ddb376504dec48081c8dc053b2313065d1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-04 12:55:14 +0200

    Add files via upload

---
+++ 08.FactorialDivision.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        //ACTION
+        double firstNumFactorial = (double)(NumberFactorial(firstNumber));
+        double secondNumFactorial = (double)NumberFactorial(secondNumber);
+
+        //OUTPUT
+        Console.WriteLine($"{firstNumFactorial / secondNumFactorial:f2}");
+    }
+
+    //ACTION
+    static double NumberFactorial(int number)
+    {
+        double result = 1.00;
+
+        for (int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+}
