4da679228be4f2c4f308ca746c70ef8c5dc13ab8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-07 19:27:18 +0200

    Add files via upload

---
+++ 13. Methods - More Exercise/04.TribonacciSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class Program
+{
+    static void Main()
+    {
+        //INPUT
+        int num = int.Parse(Console.ReadLine());
+        int[] arrayNumbers = new int[num];
+
+        //OUTPUT
+        Console.WriteLine(String.Join(" ", TribonacciSequence(num, arrayNumbers)));
+    }
+
+    //ACTION
+    static int[] TribonacciSequence(int num, int[] arrayNumbers)
+    {
+        arrayNumbers = new int[num];
+        arrayNumbers[0] = 1;
+
+        for (int i = 1; i < num; i++)
+        {
+            if (i == 1)
+            {
+                arrayNumbers[i] = i;
+            }
+            else if (i == 2)
+            {
+                arrayNumbers[i] = i;
+            }
+            else
+            {
+                arrayNumbers[i] = arrayNumbers[i - 1] + arrayNumbers[i - 2] + arrayNumbers[i - 3];
+            }
+        }
+
+        //OUTPUT
+        return arrayNumbers;
+    }
+}
