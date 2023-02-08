9273d210d196bee36e4ada8210b2562ac92a096d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 00:11:32 +0200

    Add files via upload

---
+++ 19. Mid Exam Archive 1/01.SoftuniReception.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01.SoftuniReception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            int studentsPerHourOne = int.Parse(Console.ReadLine());
+            int studentsPerHourTwo = int.Parse(Console.ReadLine());
+            int studentsPerHourThree = int.Parse(Console.ReadLine());
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            //ACTION
+            int studentsPerHourSum = studentsPerHourOne + studentsPerHourTwo + studentsPerHourThree;
+
+            int currentStudentsCount = studentsCount;
+            int hoursCount = 0;
+
+            //Hours Counter
+            while (currentStudentsCount > 0)
+            {
+                hoursCount++;
+                currentStudentsCount -= studentsPerHourSum;
+                if (hoursCount % 4 == 0)
+                {
+                    hoursCount++;
+                }
+            }
+
+            //OUTPUT
+            Console.WriteLine($"Time needed: {hoursCount}h.");
+        }
+    }
+}
