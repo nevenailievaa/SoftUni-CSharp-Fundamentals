e0dd419418c4c8b6fcfa53dc337b71e39eadd87a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:40:07 +0200

    Add files via upload

---
+++ 14. List - Lab/06.ListManipulationBasics.cs
@@ -0,0 +1,42 @@
+﻿//INPUT
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//ACTION
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "end")
+{
+	if (command.Contains("Add") == true)
+	{
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.Add(value);
+	}
+    else if (command.Contains("RemoveAt") == true)
+    {
+        string[] commandArray = command.Split();
+        int index = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.RemoveAt(index);
+    }
+    else if (command.Contains("Remove") == true)
+    {
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.Remove(value);
+    }
+    else if (command.Contains("Insert") == true)
+    {
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+        int index = int.Parse(commandArray[2]);
+
+        //Return Value:
+        numbers.Insert(index, value);
+    }
+}
+Console.WriteLine(String.Join(" ", numbers));
