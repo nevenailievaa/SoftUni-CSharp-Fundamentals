5a99b6c03b019fb927dc81227019dcaeb05b84db
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:39:53 +0200

    Add files via upload

---
+++ 14. List - Lab/05.RemoveNegativesAndReverse.cs
@@ -0,0 +1,28 @@
+﻿//INPUT
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+int numbersCnt = numbers.Count;
+
+//CHANGEABLE DATA
+int negativeNumsCnt = 0;
+
+//ACTION
+for (int i = 0; i < numbers.Count; i++)
+{
+	if (numbers[i] < 0)
+	{
+		negativeNumsCnt += 1;
+		numbers.RemoveAt(i);
+		i -= 1;
+	}
+}
+
+//OUTPUT
+if (negativeNumsCnt == numbersCnt)
+{
+	Console.WriteLine("empty");
+}
+else
+{
+	numbers.Reverse();
+	Console.WriteLine(String.Join(" ", numbers));
+}
