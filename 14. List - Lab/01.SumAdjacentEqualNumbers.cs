a728612aca47385cc1dc681589586fa2bf6c3f5e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:39:01 +0200

    Add files via upload

---
+++ 14. List - Lab/01.SumAdjacentEqualNumbers.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+List<double> numbers = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+//ACTION
+for (int i = 0; i < numbers.Count -1; i++)
+{
+    if (numbers[i] == numbers[i+1])
+    {
+        numbers[i] += numbers[i+1];
+        numbers.RemoveAt(i+1);
+        i = -1;
+    }
+}
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", numbers));
