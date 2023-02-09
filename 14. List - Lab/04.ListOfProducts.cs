ed005026beb5eab312e80cec09baab5d4dbefc3e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:39:39 +0200

    Add files via upload

---
+++ 14. List - Lab/04.ListOfProducts.cs
@@ -0,0 +1,20 @@
+﻿//INPUT
+int productsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+List<string> productsList = new List<string>();
+
+//Products Input
+for (int i = 0; i < productsCount; i++)
+{
+    productsList.Add(Console.ReadLine());
+}
+
+//Order By Name
+productsList.Sort();
+
+//OUTPUT
+for (int j = 1; j <= productsCount; j++)
+{
+    Console.WriteLine($"{j}.{productsList[j-1]}");
+}
