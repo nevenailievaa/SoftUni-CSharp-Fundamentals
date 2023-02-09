356c8222cb5159cae7f9abe083606d0e6dcf2a53
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:40:21 +0200

    Add files via upload

---
+++ 14. List - Lab/07.ListManipulationAdvanced.cs
@@ -0,0 +1,147 @@
+﻿//INPUT
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//ACTION
+string command = string.Empty;
+bool madeChanges = false;
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command.Contains("Add") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.Add(value);
+    }
+    else if (command.Contains("RemoveAt") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int index = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.RemoveAt(index);
+    }
+    else if (command.Contains("Remove") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        //Return Value:
+        numbers.Remove(value);
+    }
+    else if (command.Contains("Insert") == true)
+    {
+        madeChanges = true;
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+        int index = int.Parse(commandArray[2]);
+
+        //Return Value:
+        numbers.Insert(index, value);
+    }
+    else if (command.Contains("Contains") == true)
+    {
+        string[] commandArray = command.Split();
+        int value = int.Parse(commandArray[1]);
+
+        //Return Value:
+        if (numbers.Contains(value))
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No such number");
+        }
+    }
+    else if (command == "PrintEven")
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+    else if (command == "PrintOdd")
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] % 2 == 1)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+        Console.WriteLine();
+    }
+    else if (command == "GetSum")
+    {
+        int numsSum = 0;
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            numsSum += numbers[i];
+        }
+        Console.WriteLine(numsSum);
+    }
+    else if (command.Contains("Filter") == true)
+    {
+        string[] commandArray = command.Split();
+        string condition = commandArray[1];
+        int value = int.Parse(commandArray[2]);
+
+        //Return Value
+        if (condition == "<")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] < value)
+                {
+                    Console.Write($"{numbers[i]}");
+                }
+            }
+        }
+        else if (condition == ">")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > value)
+                {
+                    Console.Write($"{numbers[i]}");
+                }
+            }
+        }
+        else if (condition == ">=")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] >= value)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+            }
+            Console.WriteLine();
+        }
+        else if (condition == "<=")
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] <= value)
+                {
+                    Console.Write($"{numbers[i]}");
+                }
+            }
+        }
+    }
+}
+
+//Original List Changes Checker
+if (madeChanges == true)
+{
+    Console.WriteLine(String.Join(" ", numbers));
+}
