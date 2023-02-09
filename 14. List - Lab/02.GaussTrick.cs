0b1ae22fea3cd339d4ec63d0e8578304997e8f63
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-09 20:39:10 +0200

    Add files via upload

---
+++ 14. List - Lab/02.GaussTrick.cs
@@ -0,0 +1,14 @@
+﻿//INPUT
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//ACTION
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (i != numbers.Count-1)
+    {
+        //First + Last
+        numbers[i] += numbers[numbers.Count-1];
+        numbers.RemoveAt(numbers.Count-1);
+    }
+}
+Console.WriteLine(String.Join(" ", numbers));
