8c95a0bda1ca37be0dc2b096160a1918e4772436
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-10 18:22:27 +0200

    Add files via upload

---
+++ 15. List - Exercise/05.BombNumbers.cs
@@ -0,0 +1,52 @@
+﻿//INPUT
+List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+string[] bombAndPower = Console.ReadLine().Split();
+
+//ACTION
+int bomb = Convert.ToInt32(bombAndPower[0].ToString());
+int power = Convert.ToInt32(bombAndPower[1].ToString());
+
+//Bomb Finder
+int bombIndex;
+int bombsCount = 0;
+
+for (int i = 0; i < numbers.Count; i++)
+{
+	if (numbers[i] == bomb)
+	{
+		//Left Side
+		for (int j = i; j >= i - power; j--)
+		{
+			if (j < 0)
+			{
+				break;
+			}
+			else
+			{
+				numbers.RemoveAt(j);
+			}
+		}
+        //Right Side
+        for (int k = i-power; k < i; k++)
+        {
+            if (k >= numbers.Count)
+            {
+                break;
+            }
+            else
+            {
+                numbers.RemoveAt(k);
+            }
+        }
+    }
+}
+
+//Remaining Numbers Sum
+int numsSum = 0;
+for (int s = 0; s < numbers.Count; s++)
+{
+	numsSum += numbers[s];
+}
+
+//OUTPUT
+Console.WriteLine(numsSum);
