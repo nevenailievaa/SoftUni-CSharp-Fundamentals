741743ff51cfe80e286145dbc12899f7f4dba561
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-10 16:50:04 +0200

    Add files via upload

---
+++ 15. List - Exercise/02.ChangeList.cs
@@ -0,0 +1,33 @@
+﻿//INPUT
+List <int> inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+//ACTION
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "end")
+{
+	if (command.Contains("Delete"))
+	{
+		string[] commandArray = command.Split();
+		int numberForDelete = Convert.ToInt32(commandArray[1].ToString());
+
+        for (int i = 0; i < inputList.Count; i++)
+		{
+			if (inputList[i] == numberForDelete)
+			{
+				inputList.Remove(inputList[i]);
+			}
+		}
+	}
+	else if (command.Contains("Insert"))
+    {
+        string[] commandArray = command.Split();
+        int element = int.Parse(commandArray[1].ToString());
+        int index = int.Parse(commandArray[2].ToString());
+
+		inputList.Insert(index, element);
+    }
+}
+
+//OUTPUT
+Console.WriteLine(string.Join(" ", inputList));
