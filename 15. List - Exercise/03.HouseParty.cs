9f9b3f974ba4e5f8cbb1bc3a039bd5e0f2fbfe7e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-10 16:50:17 +0200

    Add files via upload

---
+++ 15. List - Exercise/03.HouseParty.cs
@@ -0,0 +1,40 @@
+﻿//INPUT
+int numberCommands = int.Parse(Console.ReadLine());
+List<string> guests = new List<string>();
+
+//ACTION
+for (int i = 0; i < numberCommands; i++)
+{
+    string command = Console.ReadLine();
+
+    string[] commandArray = command.Split().ToArray();
+    string guestName = commandArray[0].ToString();
+
+    if (command.Contains("is going!"))
+    {
+        if (guests.Contains(guestName))
+        {
+            Console.WriteLine($"{guestName} is already in the list!");
+        }
+        else
+        {
+            guests.Add(guestName);
+        }
+    }
+    else if (command.Contains("is not going!"))
+    {
+        if (guests.Contains(guestName))
+        {
+            guests.Remove(guestName);
+        }
+        else
+        {
+            Console.WriteLine($"{guestName} is not in the list!");
+        }
+    }
+}
+//OUTPUT
+for (int k = 0; k < numberCommands; k++)
+{
+    Console.WriteLine(guests[k]);
+}
