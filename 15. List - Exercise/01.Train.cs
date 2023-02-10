34535f18bda55f26af2efa32f3a29f07516a46f6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-10 15:41:28 +0200

    Add files via upload

---
+++ 15. List - Exercise/01.Train.cs
@@ -0,0 +1,30 @@
+﻿//INPUT
+List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+int wagonsCapacity = int.Parse(Console.ReadLine());
+
+//ACTION
+string command = String.Empty;
+
+while ((command = Console.ReadLine()) != "end")
+{
+	if (command.Contains("Add"))
+	{
+		int passengersAdd = int.Parse(command.Split()[1]);
+		wagons.Add(passengersAdd);
+	}
+	else
+	{
+		int inputPassengers = Convert.ToInt32(command);
+		for (int i = 0; i < wagons.Count; i++)
+		{
+			if (inputPassengers <= wagonsCapacity - wagons[i])
+			{
+				wagons[i] += inputPassengers;
+				break;
+			}
+		}
+	}
+}
+
+//OUTPUT
+Console.Write(string.Join(" ", wagons));
