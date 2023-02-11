a54f11fb5a89e8fb4dde254e9f59841f196ddd10
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-11 13:25:40 +0200

    Add files via upload

---
+++ 15. List - Exercise/07.AppendArrays.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+using System;
+
+List<string> inputList = Console.ReadLine().Split("|", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+//ACTION
+List<int> outputList = new List<int>();
+
+for (int i = 0; i < inputList.Count; i++)
+{
+    string currentNum = inputList[i];
+    int[] currentNumbersArray = currentNum.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+    for (int j = 0; j < currentNumbersArray.Length; j++)
+    {
+        outputList.Insert(j, currentNumbersArray[j]);
+    }
+}
+
+for (int j = 0; j < outputList.Count; j++)
+{
+    Console.Write($"{outputList[j]} ");
+}
