268d9eec23ef8ee60396479fd8e1915f481a3f5e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-12 21:52:10 +0200

    Add files via upload

---
+++ 20. Mid Exam Archive 2/03.HeartDelivery.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HeartDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INPUT
+            int[] neighbourHood = Console.ReadLine().Split("@").Select(int.Parse).ToArray();
+
+            //ACTION
+            string command = Console.ReadLine();
+            int houseIndex = 0;
+
+            while (command.Contains("Jump"))
+            {
+                string[] commandArray = command.Split();
+                int currentJumpLength = Convert.ToInt32(commandArray[1]);
+
+                houseIndex += currentJumpLength;
+
+                if (houseIndex >= neighbourHood.Length || houseIndex < 0)
+                {
+                    houseIndex = 0;
+                }
+
+                if (neighbourHood[houseIndex] == 0)
+                {
+                    Console.WriteLine($"Place {houseIndex} already had Valentine's day.");
+                }
+                else
+                {
+                    neighbourHood[houseIndex] -= 2;
+                    if (neighbourHood[houseIndex] == 0)
+                    {
+                        Console.WriteLine($"Place {houseIndex} has Valentine's day.");
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            //Failed Places Counter
+            int failedPlacesCount = 0;
+
+            for (int i = 0; i < neighbourHood.Length; i++)
+            {
+                if (neighbourHood[i] != 0)
+                {
+                    failedPlacesCount++;
+                }
+            }
+
+            //OUTPUT
+            int lastPosition = houseIndex;
+            Console.WriteLine($"Cupid's last position was {lastPosition}.");
+
+            if (failedPlacesCount == 0)
+            {
+                Console.WriteLine("Mission was successful.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid has failed {failedPlacesCount} places.");
+            }
+        }
+    }
+}
