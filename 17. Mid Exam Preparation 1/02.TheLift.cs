3e7044c9233f12648c2ec511cbd63d3a4c0a9202
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-14 14:15:25 +0200

    Add files via upload

---
+++ 17. Mid Exam Preparation 1/02.TheLift.cs
@@ -0,0 +1,54 @@
+﻿//INPUT
+int peopleWaitingForTheLift = int.Parse(Console.ReadLine());
+int[] liftArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+for (int i = 0; i < liftArray.Length; i++)
+{
+	if (peopleWaitingForTheLift == 0)
+	{
+		break;
+	}
+    while (liftArray[i] < 4 && liftArray[i] >= 0)
+    {
+        if (peopleWaitingForTheLift == 0)
+        {
+            break;
+        }
+		else
+		{
+            peopleWaitingForTheLift--;
+            liftArray[i]++;
+        }
+    }
+}
+
+//Empty Spots Checker
+bool emptySpots = false;
+for (int j = 0; j < liftArray.Length; j++)
+{
+	if (liftArray[j] < 4 && liftArray[j] >= 0)
+	{
+		emptySpots = true;
+		break;
+	}
+}
+
+//OUTPUT
+if (peopleWaitingForTheLift == 0 || emptySpots == false)
+{
+	if (peopleWaitingForTheLift == 0 && emptySpots == true)
+	{
+		Console.WriteLine("The lift has empty spots!");
+		Console.WriteLine(String.Join(' ', liftArray));
+    }
+	else if (peopleWaitingForTheLift > 0 && emptySpots == false)
+	{
+		Console.WriteLine($"There isn't enough space! {peopleWaitingForTheLift} people in a queue!");
+		Console.WriteLine(String.Join(' ', liftArray));
+	}
+	else if (peopleWaitingForTheLift == 0 && emptySpots == false)
+	{
+        Console.WriteLine(String.Join(' ', liftArray));
+    }
+}
