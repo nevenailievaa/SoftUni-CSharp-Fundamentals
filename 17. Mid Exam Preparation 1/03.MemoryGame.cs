c572815db528ae04bde8e43aa0a76ff335cdc836
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-14 15:15:42 +0200

    Add files via upload

---
+++ 17. Mid Exam Preparation 1/03.MemoryGame.cs
@@ -0,0 +1,64 @@
+﻿//INPUT
+List<string> elementsList = Console.ReadLine().Split().ToList();
+string command = string.Empty;
+int movesCount = 0;
+
+//ACTION
+while ((command = Console.ReadLine()) != "end")
+{
+    movesCount++;
+    string[] commandArray = command.Split();
+    int indexOne = Convert.ToInt32(commandArray[0].ToString());
+    int indexTwo = Convert.ToInt32(commandArray[1].ToString());
+
+    //Invalid Index Input
+    if (indexOne == indexTwo || indexOne < 0 || indexOne >= elementsList.Count || indexTwo < 0 || indexTwo >= elementsList.Count)
+    {
+        int middleIndex = elementsList.Count / 2;
+        elementsList.Insert(middleIndex, $"-{movesCount}a");
+        elementsList.Insert(middleIndex, $"-{movesCount}a");
+        Console.WriteLine("Invalid input! Adding additional elements to the board");
+    }
+    else
+    {
+        //Two Matching Elements
+        if (elementsList[indexOne] == elementsList[indexTwo])
+        {
+            Console.WriteLine($"Congrats! You have found matching elements - {elementsList[indexOne]}!");
+
+            //Indexes Checker
+            if (indexOne < indexTwo)
+            {
+                elementsList.RemoveAt(indexOne);
+                elementsList.RemoveAt(indexTwo-1);
+            }
+            else
+            {
+                elementsList.RemoveAt(indexOne);
+                elementsList.RemoveAt(indexTwo);
+            }
+        }
+        //Two Not Matching Elements
+        else if (elementsList[indexOne] != elementsList[indexTwo])
+        {
+            Console.WriteLine("Try again!");
+        }
+
+        //Elements List Length Ckecker
+        if (elementsList.Count == 0)
+        {
+            break;
+        }
+    }
+}
+
+//OUTPUT
+if (elementsList.Count == 0)
+{
+    Console.WriteLine($"You have won in {movesCount} turns!");
+}
+else
+{
+    Console.WriteLine("Sorry you lose :(");
+    Console.WriteLine(String.Join(" ", elementsList));
+}
