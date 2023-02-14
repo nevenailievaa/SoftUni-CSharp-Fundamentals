41c6a5a7c5159ef8e42635fc2c81428f5562e0e1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-14 13:13:20 +0200

    Add files via upload

---
+++ 17. Mid Exam Preparation 1/01.ComputerStore.cs
@@ -0,0 +1,50 @@
+﻿//INPUT
+string command = Console.ReadLine();
+double totalPriceWithoutTaxes = 0;
+double totalPriceWithTaxes = 0;
+double taxes = 0;
+
+//ACTION
+while (command != "special" && command != "regular")
+{
+    double currentPrice = double.Parse(command);
+
+    if (currentPrice <= 0)
+	{
+		Console.WriteLine("Invalid price!");
+	}
+	else
+	{
+		totalPriceWithoutTaxes += currentPrice;
+		totalPriceWithTaxes += (currentPrice * 0.2) + currentPrice;
+		taxes += (currentPrice * 0.2);
+    }
+
+	command = Console.ReadLine();
+}
+
+if (command == "special")
+{
+	totalPriceWithTaxes *= 0.9;
+
+	if (totalPriceWithTaxes <= 0)
+	{
+		Console.WriteLine("Invalid order!");
+		return;
+	}
+}
+else if (command == "regular")
+{
+    if (totalPriceWithTaxes <= 0)
+    {
+        Console.WriteLine("Invalid order!");
+        return;
+    }
+}
+
+//OUTPUT
+Console.WriteLine("Congratulations you've just bought a new computer!");
+Console.WriteLine($"Price without taxes: {totalPriceWithoutTaxes:f2}$");
+Console.WriteLine($"Taxes: {taxes:f2}$");
+Console.WriteLine("-----------");
+Console.WriteLine($"Total price: {totalPriceWithTaxes:f2}$");
