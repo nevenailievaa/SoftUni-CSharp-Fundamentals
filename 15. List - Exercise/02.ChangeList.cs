f0ed6b74e9e238910f03759c7b10eff90e42917c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-16 13:31:29 +0200

    Update 02.ChangeList.cs

--- 15. List - Exercise/02.ChangeList.cs
+++ 15. List - Exercise/02.ChangeList.cs
@@ -1,33 +1,46 @@
-﻿//INPUT
-List <int> inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
-
-//ACTION
-string command = string.Empty;
-
-while ((command = Console.ReadLine()) != "end")
+namespace ConsoleApp1
 {
-	if (command.Contains("Delete"))
-	{
-		string[] commandArray = command.Split();
-		int numberForDelete = Convert.ToInt32(commandArray[1].ToString());
-
-        for (int i = 0; i < inputList.Count; i++)
-		{
-			if (inputList[i] == numberForDelete)
-			{
-				inputList.Remove(inputList[i]);
-			}
-		}
-	}
-	else if (command.Contains("Insert"))
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+ 
+    class Program
     {
-        string[] commandArray = command.Split();
-        int element = int.Parse(commandArray[1].ToString());
-        int index = int.Parse(commandArray[2].ToString());
-
-		inputList.Insert(index, element);
+        static void Main(string[] args)
+        {
+            //INPUT
+            List<int> inputList = Console.ReadLine().Split().Select(int.Parse).ToList();
+ 
+            //ACTION
+            string command = string.Empty;
+ 
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command.Contains("Delete"))
+                {
+                    string[] commandArray = command.Split();
+                    int numberForDelete = Convert.ToInt32(commandArray[1].ToString());
+ 
+                    for (int i = inputList.Count - 1; i >= 0; i--)
+                    {
+                        if (inputList[i] == numberForDelete)
+                        {
+                            inputList.Remove(inputList[i]);
+                        }
+                    }
+                }
+                else if (command.Contains("Insert"))
+                {
+                    string[] commandArray = command.Split();
+                    int element = int.Parse(commandArray[1].ToString());
+                    int index = int.Parse(commandArray[2].ToString());
+ 
+                    inputList.Insert(index, element);
+                }
+            }
+ 
+            //OUTPUT
+            Console.WriteLine(string.Join(" ", inputList));
+        }
     }
 }
-
-//OUTPUT
-Console.WriteLine(string.Join(" ", inputList));
