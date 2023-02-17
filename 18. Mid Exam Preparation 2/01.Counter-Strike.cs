c2936fa12dac58d36827c795cc7b07b5a38a9218
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-17 14:18:05 +0200

    Add files via upload

---
+++ 18. Mid Exam Preparation 2/01.Counter-Strike.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+int energy = int.Parse(Console.ReadLine());
+int wonBattles = 0;
+string command = string.Empty;
+
+//ACTION
+while ((command = Console.ReadLine()) != "End of battle")
+{
+    int distance = int.Parse(command);
+
+    if (distance <= energy)
+    {
+        wonBattles++;
+        energy -= distance;
+
+        if (wonBattles % 3 == 0)
+        {
+            energy += wonBattles;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Not enough energy! Game ends with {wonBattles} won battles and {energy} energy");
+        return;
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"Won battles: {wonBattles}. Energy left: {energy}");
