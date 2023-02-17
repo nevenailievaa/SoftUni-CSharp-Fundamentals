f99d334ad3c638137d34174a430d9fbe743a3c1a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-17 14:18:16 +0200

    Add files via upload

---
+++ 18. Mid Exam Preparation 2/02.ShootForTheWin.cs
@@ -0,0 +1,34 @@
+﻿//INPUT
+int[] targets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+string command = string.Empty;
+int shotTargets = 0;
+
+while ((command = Console.ReadLine()) != "End")
+{
+    int index = int.Parse(command);
+	int value;
+
+	if (index >= 0 && index < targets.Length && targets[index] != -1)
+	{
+		shotTargets++;
+		value = targets[index];
+		targets[index] = -1;
+
+		for (int i = 0; i < targets.Length; i++)
+		{
+			if (targets[i] != -1 && targets[i] > value)
+			{
+				targets[i] -= value;
+            }
+			else if (targets[i] != -1 && targets[i] <= value)
+            {
+                targets[i] += value;
+            }
+		}
+	}
+}
+
+//OUTPUT
+Console.WriteLine($"Shot targets: {shotTargets} -> {string.Join(" ", targets)}");
