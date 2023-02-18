58d035d60abea9ec1a9116d0a2e12b3f0726eb10
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-18 14:00:04 +0200

    Add files via upload

---
+++ 22. Mid Exam Archive 4/01.BlackFlag.cs
@@ -0,0 +1,31 @@
+﻿//INPUT
+int days = int.Parse(Console.ReadLine());
+int dailyPlunder = int.Parse(Console.ReadLine());
+double expectedPlunder = double.Parse(Console.ReadLine());
+
+//ACTION
+double allPlunder = 0;
+for (int i = 1; i <= days; i++)
+{
+    allPlunder += dailyPlunder;
+
+	if (i % 3 == 0)
+	{
+        allPlunder += (double)dailyPlunder / 2;
+    }
+	if (i % 5 == 0)
+	{
+		allPlunder *= 0.7;
+	}
+}
+
+//OUTPUT
+if (allPlunder >= expectedPlunder)
+{
+	Console.WriteLine($"Ahoy! {allPlunder:f2} plunder gained.");
+}
+else
+{
+	double collectedPercentage = (allPlunder / expectedPlunder) * 100;
+	Console.WriteLine($"Collected only {collectedPercentage:f2}% of the plunder.");
+}
