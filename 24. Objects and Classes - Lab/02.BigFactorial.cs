84a8214492893fa2065f9ec135a6e4c37b3bdd1d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-24 15:39:50 +0200

    Add files via upload

---
+++ 24. Objects and Classes - Lab/02.BigFactorial.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+using System.Numerics;
+
+int inputNumber = int.Parse(Console.ReadLine());
+
+//ACTION
+BigInteger sum = 1;
+
+for (int i = inputNumber; i >= 1; i--)
+{
+    sum *= i;
+}
+
+//OUTPUT
+Console.WriteLine(sum);
