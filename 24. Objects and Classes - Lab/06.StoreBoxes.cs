af370e316e748bf2214f8ad918d4975a522a060f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-25 00:29:18 +0200

    Add files via upload

---
+++ 24. Objects and Classes - Lab/06.StoreBoxes.cs
@@ -0,0 +1,55 @@
+﻿//INPUT
+string info = string.Empty;
+
+//ACTION
+List<Box> boxesList = new List<Box>();
+
+while ((info = Console.ReadLine()) != "end")
+{
+    string[] infoAsArray = info.Split();
+
+    int serialNumber = int.Parse(infoAsArray[0]);
+    string itemName = infoAsArray[1];
+    int itemQuantity = int.Parse(infoAsArray[2]);
+    decimal ItemPrice = decimal.Parse(infoAsArray[3]);
+
+    Item item = new Item
+    {
+        Name = itemName,
+        Price = ItemPrice
+    };
+
+    Box box = new Box
+    {
+        SerialNumber = serialNumber,
+        Item = item,
+        ItemQuantity = itemQuantity,
+        PriceForBox = ItemPrice * itemQuantity
+    };
+
+    boxesList.Add(box);
+}
+
+//Order By Descending
+foreach (var box in boxesList.OrderByDescending(x => x.PriceForBox))
+{
+    Console.WriteLine(box.SerialNumber);
+    Console.WriteLine($"-- {box.Item.Name} - ${box.Item.Price:f2}: {box.ItemQuantity}");
+    Console.WriteLine($"-- ${box.PriceForBox:f2}");
+}
+
+//Class Box
+public class Box
+{
+    public int SerialNumber { get; set; }
+    public Item Item { get; set; }
+    public int ItemQuantity { get; set; }
+    public decimal PriceForBox { get; set; }
+}
+
+//Class Item
+public class Item
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
