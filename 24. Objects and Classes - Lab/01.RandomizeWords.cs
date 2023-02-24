660910caaf1c7a1fbb263792c77723fadd27b1d4
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-24 15:27:42 +0200

    Add files via upload

---
+++ 24. Objects and Classes - Lab/01.RandomizeWords.cs
@@ -0,0 +1,24 @@
+﻿//INPUT
+string[] words = Console.ReadLine().Split().ToArray();
+
+//ACTION
+for (int i = 0; i < words.Length - 1; i++)
+{
+    Random random = new Random();
+    int randomIndex = random.Next(0, words.Length);
+
+    string currentWord = words[i];
+
+    while (i == randomIndex)
+    {
+        randomIndex = random.Next(0, words.Length);
+    }
+
+    string wordSwap = words[randomIndex];
+
+    words[i] = wordSwap;
+    words[randomIndex] = currentWord;
+}
+
+//OUTPUT
+Console.WriteLine(string.Join(Environment.NewLine, words));
