d18bd4206842f0c9087cf84bf1d65147dd6d3334
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-24 17:08:35 +0200

    Add files via upload

---
+++ 24. Objects and Classes - Lab/04.Students.cs
@@ -0,0 +1,37 @@
+﻿//ACTION
+string command = String.Empty;
+List<Student> students = new List<Student>();
+
+//INPUT
+while ((command = Console.ReadLine()) != "end") 
+{
+    string[] studentInfoArray = command.Split();
+    Student currentStudent = new Student();
+
+    currentStudent.FirstName = studentInfoArray[0];
+    currentStudent.LastName = studentInfoArray[1];
+    currentStudent.Age = int.Parse(studentInfoArray[2]);
+    currentStudent.HomeTown = studentInfoArray[3];
+
+    students.Add(currentStudent);
+}
+
+//OUTPUT
+string outputCity = Console.ReadLine();
+
+foreach (Student currentStudent in students)
+{
+    if (currentStudent.HomeTown == outputCity)
+    {
+        Console.WriteLine($"{currentStudent.FirstName} {currentStudent.LastName} is {currentStudent.Age} years old.");
+    }
+}
+
+//STUDENT CLASS
+public class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string HomeTown { get; set; }
+}
