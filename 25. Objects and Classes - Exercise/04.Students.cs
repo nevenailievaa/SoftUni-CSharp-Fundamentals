23575074fb4b5baf7ca2be2a5556eeeb2eb91855
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-25 14:20:39 +0200

    Add files via upload

---
+++ 25. Objects and Classes - Exercise/04.Students.cs
@@ -0,0 +1,43 @@
+﻿//INPUT
+int studentsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+List<Students> studentsList = new List<Students>();
+
+for (int i = 1; i <= studentsCount; i++)
+{
+    string[] studentInfoArray = Console.ReadLine().Split();
+
+    string firstName = studentInfoArray[0];
+    string lastName = studentInfoArray[1];
+    double grade = double.Parse(studentInfoArray[2]);
+
+    studentsList.Add(new Students(firstName, lastName, grade));
+}
+
+//OUTPUT
+foreach (Students currentStudent in studentsList.OrderByDescending(x => x.Grade))
+{
+    Console.WriteLine(currentStudent);
+}
+
+//Students Class
+public class Students
+{
+    public Students(string firstName, string lastName, double grade)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Grade = grade;
+    }
+
+    //Return Value
+    public override string ToString()
+    {
+        return ($"{this.FirstName} {this.LastName}: {Grade:f2}");
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+}
