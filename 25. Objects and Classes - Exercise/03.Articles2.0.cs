7a26192336852e93e0f637df1248c06c39c83c7e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-25 14:02:02 +0200

    Add files via upload

---
+++ 25. Objects and Classes - Exercise/03.Articles2.0.cs
@@ -0,0 +1,45 @@
+﻿//INPUT
+List<Article> articleList = new List<Article>();
+int commandsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+for (int i = 1; i <= commandsCount; i++)
+{
+    string[] inputs = Console.ReadLine().Split(", ");
+
+    string title = inputs[0];
+    string content = inputs[1];
+    string author = inputs[2];
+
+    Article article = new Article(title, content, author);
+
+    articleList.Add(article);
+}
+
+string command = Console.ReadLine();
+
+//OUTPUT
+foreach (var currentArticle in articleList)
+{
+    Console.WriteLine(currentArticle);
+}
+
+//Article Class
+class Article
+{
+    public Article(string title, string content, string author)
+    {
+        Title = title;
+        Content = content;
+        Author = author;
+    }
+
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public override string ToString()
+    {
+        return $"{this.Title} - {this.Content}: {this.Author}";
+    }
+}
