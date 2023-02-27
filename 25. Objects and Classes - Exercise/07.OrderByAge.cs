2e5d9eafb1426f56f7bb5c18f14d23f771db23ef
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-02-27 11:52:23 +0200

    Add files via upload

---
+++ 25. Objects and Classes - Exercise/07.OrderByAge.cs
@@ -0,0 +1,53 @@
+﻿//INPUT
+string info = string.Empty;
+
+//ACTION
+List<Student> studentsList = new List<Student>();
+
+while ((info = Console.ReadLine()) != "End")
+{
+    string[] studentInfo = info.Split();
+
+    string name = studentInfo[0];
+    string studentId = studentInfo[1];
+    int age = int.Parse(studentInfo[2]);
+
+    Student currentStudent = new Student(name, studentId, age);
+
+    //Already Used ID Checker
+    bool alreadyAnId = false;
+    for (int i = 0; i < studentsList.Count; i++)
+    {
+        if (studentsList[i].StudentID == currentStudent.StudentID)
+        {
+            studentsList[i].Name = currentStudent.Name;
+            studentsList[i].Age = currentStudent.Age;
+            alreadyAnId = true;
+        }
+    }
+
+    if (!alreadyAnId)
+    {
+        studentsList.Add(currentStudent);
+    }
+}
+
+//OUTPUT
+foreach (Student student in studentsList.OrderBy(s => s.Age))
+{
+    Console.WriteLine($"{student.Name} with ID: {student.StudentID} is {student.Age} years old.");
+}
+
+//STUDENT CLASS
+public class Student
+{
+    public Student (string name, string studentId, int age)
+    {
+        Name = name;
+        StudentID = studentId;
+        Age = age;
+    }
+    public string Name { get; set; }
+    public string StudentID { get; set; }
+    public int Age { get; set; }
+}
