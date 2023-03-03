f76f8c3e75df41f46d8b232b62944db36ccfce69
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-03 16:45:01 +0200

    Add files via upload

---
+++ 27. Associative Arrays - Lab/03.WordSynonyms.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+int wordsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+
+for (int i = 0; i < wordsCount; i++)
+{
+	string word = Console.ReadLine();
+	string synonym = Console.ReadLine();
+
+    if (!words.ContainsKey(word))
+	{
+        List<string> synonymsList = new List<string>();
+
+        synonymsList.Add(synonym);
+		words.Add(word, synonymsList);
+	}
+	else
+	{
+        words[word].Add(synonym);
+	}
+}
+
+//OUTPUT
+foreach (var currentWordAndSynonym in words)
+{
+	Console.WriteLine($"{currentWordAndSynonym.Key} - {String.Join(", ", currentWordAndSynonym.Value)}");
+}
