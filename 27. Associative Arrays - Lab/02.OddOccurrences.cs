f0505897036129f2cb499bd95fa936e3751e2d1c
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-03 16:07:10 +0200

    Add files via upload

---
+++ 27. Associative Arrays - Lab/02.OddOccurrences.cs
@@ -0,0 +1,26 @@
+﻿//INPUT
+string[] inputWords = Console.ReadLine().ToLower().Split().ToArray();
+
+//ACTION
+Dictionary<string, int> wordsDictionary = new Dictionary<string, int>();
+
+foreach (var word in inputWords)
+{
+    if (!wordsDictionary.ContainsKey(word))
+    {
+        wordsDictionary.Add(word, 1);
+    }
+    else
+    {
+        wordsDictionary[word]++;
+    }
+}
+
+//OUTPUT
+foreach (var word in wordsDictionary)
+{
+    if (word.Value % 2 == 1)
+    {
+        Console.Write($"{word.Key} ");
+    }
+}
