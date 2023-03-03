8b2c940481ddfddd959c7b09faf2f35be579f136
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-03 20:06:59 +0200

    Add files via upload

---
+++ 28. Associative Arrays - Exercise/03.Orders.cs
@@ -0,0 +1,29 @@
+﻿//INPUT
+string info = string.Empty;
+
+//ACTION
+Dictionary<string, decimal> productsDictionary = new Dictionary<string, decimal>();
+
+while ((info = Console.ReadLine()) != "buy")
+{
+    string[] infoArray = info.Split();
+
+    string productName = infoArray[0];
+    decimal productPrice = decimal.Parse(infoArray[1]);
+    int productCount = int.Parse(infoArray[2]);
+
+    if (!productsDictionary.ContainsKey(productName))
+    {
+        productsDictionary.Add(productName, productPrice * productCount);
+    }
+    else
+    {
+        productsDictionary[productName] += productPrice * productCount;
+    }
+}
+
+//OUTPUT
+foreach (var product in productsDictionary)
+{
+    Console.WriteLine($"{product.Key} -> {product.Value:f2}");
+}
