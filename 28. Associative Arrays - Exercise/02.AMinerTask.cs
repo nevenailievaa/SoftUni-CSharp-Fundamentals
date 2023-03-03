78f241cad058a23801eadc1b0e335a89055ba75b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-03 19:56:57 +0200

    Add files via upload

---
+++ 28. Associative Arrays - Exercise/02.AMinerTask.cs
@@ -0,0 +1,25 @@
+﻿//INPUT
+string info = string.Empty;
+
+//ACTION
+Dictionary<string, int> resources = new Dictionary<string, int>();
+
+while ((info = Console.ReadLine()) != "stop")
+{
+    int quantity = int.Parse(Console.ReadLine());
+
+    if (!resources.ContainsKey(info))
+    {
+        resources.Add(info, quantity);
+    }
+    else
+    {
+        resources[info] += quantity;
+    }
+}
+
+//OUTPUT
+foreach (var resource in resources)
+{
+    Console.WriteLine($"{resource.Key} -> {resource.Value}");
+}
