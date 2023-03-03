cdc161986d3ce7c97a98598192efb7e7be98ae88
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-03 16:04:10 +0200

    Add files via upload

---
+++ 27. Associative Arrays - Lab/01.CountRealNumbers.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//ACTION
+SortedDictionary<int, int> sortedNumbers = new SortedDictionary<int, int>();
+
+foreach (int number in numbers)
+{
+	if (!sortedNumbers.ContainsKey(number))
+	{
+		sortedNumbers.Add(number, 1);
+	}
+	else
+	{
+		sortedNumbers[number] += 1;
+	}
+}
+
+//OUTPUT
+foreach (var num in sortedNumbers)
+{
+	Console.WriteLine($"{num.Key} -> {num.Value}");
+}
