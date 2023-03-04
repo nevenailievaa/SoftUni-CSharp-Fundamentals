81db2f63fd105fd28870e9ea40c76df74ecf84b4
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-04 15:04:19 +0200

    Add files via upload

---
+++ 28. Associative Arrays - Exercise/06.StudentAcademy.cs
@@ -0,0 +1,34 @@
+﻿//INPUT
+using System.Xml.Linq;
+
+int commandsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+Dictionary<string, double> students = new Dictionary<string, double>();
+Dictionary<string, double> studentsGrades = new Dictionary<string, double>();
+
+for (int i = 0; i < commandsCount; i++)
+{
+    string studentName = Console.ReadLine();
+    double studentGrade = double.Parse(Console.ReadLine());
+
+    if (!students.ContainsKey(studentName))
+    {
+        studentsGrades.Add(studentName, 1);
+        students.Add(studentName, studentGrade);
+    }
+    else
+    {
+        double gradesCount = studentsGrades[studentName] + 1;
+        students[studentName] = (students[studentName] + studentGrade) / gradesCount;
+    }
+}
+
+//OUTPUT
+foreach (var student in students)
+{
+    if (student.Value >= 4.5)
+    {
+        Console.WriteLine($"{student.Key} -> {student.Value:f2}");
+    }
+}
