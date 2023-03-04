b729881359912db30395b1ef8c8984479df4de31
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-04 15:03:45 +0200

    Add files via upload

---
+++ 28. Associative Arrays - Exercise/04.SoftUniParking.cs
@@ -0,0 +1,50 @@
+﻿//INPUT
+int commandsCount = int.Parse(Console.ReadLine());
+
+//ACTION
+Dictionary<string, string> parkedCars = new Dictionary<string, string>();
+
+for (int i = 0; i < commandsCount; i++)
+{
+    string[] commandArray = Console.ReadLine().Split();
+
+	//Register Command
+	if (commandArray[0] == "register")
+	{
+		string username = commandArray[1];
+        string plate = commandArray[2];
+
+		if (!parkedCars.ContainsKey(username))
+		{
+            parkedCars.Add(username, plate);
+			Console.WriteLine($"{username} registered {plate} successfully");
+        }
+		else
+		{
+			Console.WriteLine($"ERROR: already registered with plate number {plate}");
+		}
+    }
+
+    //Unregister Command
+    else if (commandArray[0] == "unregister")
+    {
+        string username = commandArray[1];
+
+        if (!parkedCars.ContainsKey(username))
+        {
+
+            Console.WriteLine($"ERROR: user {username} not found");
+        }
+        else
+        {
+            parkedCars.Remove(username);
+            Console.WriteLine($"{username} unregistered successfully");
+        }
+    }
+}
+
+//OUTPUT
+foreach (var user in parkedCars)
+{
+    Console.WriteLine($"{user.Key} => {user.Value}");
+}
