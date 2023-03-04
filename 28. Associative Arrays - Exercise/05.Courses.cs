948ec76e0ec8568f3a49732df9d21f987a8dbeaf
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-04 15:04:08 +0200

    Add files via upload

---
+++ 28. Associative Arrays - Exercise/05.Courses.cs
@@ -0,0 +1,34 @@
+﻿//INPUT
+string info = string.Empty;
+
+//ACTION
+Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+while ((info = Console.ReadLine()) != "end")
+{
+    string[] courseInfoArray = info.Split(" : ").ToArray();
+    string courseName = courseInfoArray[0];
+    string student = courseInfoArray[1];
+
+    if (!courses.ContainsKey(courseName))
+    {
+        List<string> studentsArray = new List<string>();
+        studentsArray.Add(student);
+        courses.Add(courseName, studentsArray);
+    }
+    else
+    {
+        courses[courseName].Add(student);
+    }
+}
+
+//OUTPUT
+foreach (var course in courses)
+{
+    Console.WriteLine($"{course.Key}: {course.Value.Count}");
+
+    for (int i = 0; i < course.Value.Count; i++)
+    {
+        Console.WriteLine($"-- {course.Value[i]}");
+    }
+}
