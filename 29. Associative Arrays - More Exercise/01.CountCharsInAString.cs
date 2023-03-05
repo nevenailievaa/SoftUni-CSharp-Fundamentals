82a49c54e44c7f5d3015c8cdd773505421a2539a
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-05 13:44:26 +0200

    Add files via upload

---
+++ 29. Associative Arrays - More Exercise/01.CountCharsInAString.cs
@@ -0,0 +1,35 @@
+﻿//INPUT
+string inputWords = Console.ReadLine();
+List<char> inputChars = new List<char>();
+inputChars.AddRange(inputWords);
+
+//ACTION
+Dictionary<char, int> charCopies = new Dictionary<char, int>();
+
+for (int i = 0; i < inputChars.Count; i++)
+{
+    //Space Checker
+    if (inputChars[i] == 32)
+    {
+        inputChars.RemoveAt(i);
+        i--;
+    }
+}
+
+foreach (var currentChar in inputChars)
+{
+    if (!charCopies.ContainsKey(currentChar))
+    {
+        charCopies.Add(currentChar, 1);
+    }
+    else
+    {
+        charCopies[currentChar] += 1;
+    }
+}
+
+//OUTPUT
+foreach (var currentChar in charCopies)
+{
+    Console.WriteLine($"{currentChar.Key} -> {currentChar.Value}");
+}
