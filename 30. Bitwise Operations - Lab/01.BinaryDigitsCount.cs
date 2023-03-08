135e3221fefd953ae14ea76fa246230a78a65b24
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-08 13:38:51 +0200

    Add files via upload

---
+++ 30. Bitwise Operations - Lab/01.BinaryDigitsCount.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+using System;
+
+int number = int.Parse(Console.ReadLine());
+int binaryDigit = int.Parse(Console.ReadLine());
+
+//ACTION
+List<int> numberInBynarySystem = new List<int>();
+int binaryDigitCounter = 0;
+
+while (number != 0)
+{
+    numberInBynarySystem.Add(number % 2);
+    if (number % 2 == binaryDigit)
+    {
+        binaryDigitCounter++;
+
+    }
+    number /= 2;
+}
+
+//OUTPUT
+Console.WriteLine(binaryDigitCounter);
