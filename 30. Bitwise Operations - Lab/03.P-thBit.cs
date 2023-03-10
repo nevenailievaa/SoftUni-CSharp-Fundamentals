8d48dfec76afa4a001f151ca9d965a91c0c548b1
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-10 15:33:19 +0200

    Add files via upload

---
+++ 30. Bitwise Operations - Lab/03.P-thBit.cs
@@ -0,0 +1,9 @@
+﻿//INPUT
+int inputNum = int.Parse(Console.ReadLine());
+int position = int.Parse(Console.ReadLine());
+
+//ACTION
+int secondNum = inputNum >> position;
+
+//OUTPUT
+Console.WriteLine(secondNum & position);
