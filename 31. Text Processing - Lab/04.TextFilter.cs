65ffe987f2d70cd9ceff25b831f6f29c70b432c8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-12 22:04:54 +0200

    Add files via upload

---
+++ 31. Text Processing - Lab/04.TextFilter.cs
@@ -0,0 +1,14 @@
+﻿//INPUT
+using System.Globalization;
+
+string[] bannedWords = Console.ReadLine().Split(", ");
+string text = Console.ReadLine();
+
+//ACTION
+foreach (var bannedWord in bannedWords)
+{
+    text = text.Replace(bannedWord, new string('*', bannedWord.Length));
+}
+
+//OUTPUT
+Console.WriteLine(text);
