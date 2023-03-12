647150b0b32d4944c0ed6f87676fe7c5891f9525
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-12 22:04:22 +0200

    Add files via upload

---
+++ 31. Text Processing - Lab/02.RepeatStrings.cs
@@ -0,0 +1,12 @@
+﻿//INPUT
+string[] stringInput = Console.ReadLine().Split();
+
+//ACTION
+for (int i = 0; i < stringInput.Length; i++)
+{
+	for (int j = 0; j < stringInput[i].Length; j++)
+	{
+        //OUTPUT
+        Console.Write(stringInput[i]);
+    }
+}
