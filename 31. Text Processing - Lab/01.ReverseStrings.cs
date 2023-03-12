2f86b72abbba23bc40a685bc6e52e25967b5a6f5
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-12 22:04:03 +0200

    Add files via upload

---
+++ 31. Text Processing - Lab/01.ReverseStrings.cs
@@ -0,0 +1,22 @@
+﻿//INPUT
+string inputString = string.Empty;
+
+//ACTION
+List<string> stringsReversed = new List<string>();
+
+while ((inputString = Console.ReadLine()) != "end")
+{
+	string reversedString = string.Empty;
+	for (int i = inputString.Length -1; i >= 0; i--)
+	{
+		reversedString += inputString[i].ToString();
+	}
+	stringsReversed.Add($"{inputString} = {reversedString}");
+}
+
+
+//OUTPUT
+for(int i = 0; i < stringsReversed.Count; i++)
+{
+    Console.WriteLine(stringsReversed[i]);
+}
