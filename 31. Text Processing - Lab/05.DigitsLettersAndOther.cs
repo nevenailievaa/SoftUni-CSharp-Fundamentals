52e01d28cbed55526747ba4c7d450a8a53d5f078
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-12 22:05:19 +0200

    Add files via upload

---
+++ 31. Text Processing - Lab/05.DigitsLettersAndOther.cs
@@ -0,0 +1,31 @@
+﻿//INPUT
+using System.Text;
+
+string inputString = Console.ReadLine();
+
+//ACTION
+StringBuilder digits = new StringBuilder();
+StringBuilder letters = new StringBuilder();
+StringBuilder symbols = new StringBuilder();
+
+for (int i = 0; i < inputString.Length; i++)
+{
+	if (Char.IsDigit(inputString[i]))
+	{
+		digits.Append(inputString[i]);
+
+    }
+	else if (Char.IsLetter(inputString[i]))
+	{
+		letters.Append(inputString[i]);
+    }
+	else
+	{
+		symbols.Append(inputString[i]);
+	}
+}
+
+//OUTPUT
+Console.WriteLine(digits);
+Console.WriteLine(letters);
+Console.WriteLine(symbols);
