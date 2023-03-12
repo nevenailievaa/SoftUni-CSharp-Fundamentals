dce5d00647181ebcc29e40b5773d0a2b1fc8a5ae
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-12 22:04:39 +0200

    Add files via upload

---
+++ 31. Text Processing - Lab/03.Substring.cs
@@ -0,0 +1,13 @@
+﻿//INPUT
+string firstString = Console.ReadLine();
+string secondString = Console.ReadLine();
+
+//ACTION
+while (secondString.Contains(firstString))
+{
+    int indexOfFirstString = secondString.IndexOf(firstString);
+    secondString = secondString.Remove(indexOfFirstString, firstString.Length);
+}
+
+//OUTPUT
+Console.WriteLine(secondString);
