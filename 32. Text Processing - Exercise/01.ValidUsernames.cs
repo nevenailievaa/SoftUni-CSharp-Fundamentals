878b61c1f8d23ec777a984a7ffaf8cd8723a19a4
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-13 13:00:11 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/01.ValidUsernames.cs
@@ -0,0 +1,54 @@
+﻿//INPUT
+string[] inputArray = Console.ReadLine().Split(", "); //95 45
+
+//ACTION
+List<string> validUsenames = new List<string>();
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    bool isValid = false;
+    if (inputArray[i].Length > 2 && inputArray[i].Length < 17)
+	{
+        char[] currentUsernameChars = inputArray[i].ToCharArray();
+
+		for (int j = 0; j < currentUsernameChars.Length; j++)
+		{
+			//Letters
+			if (Char.IsLetter(currentUsernameChars[j]))
+			{
+                isValid = true;
+            }
+            //Numbers
+			else if (Char.IsDigit(currentUsernameChars[j]))
+            {
+                isValid = true;
+            }
+            //Hyphens
+			else if (currentUsernameChars[j] == 45)
+            {
+                isValid = true;
+            }
+            //Underscores
+            else if (currentUsernameChars[j] == 95)
+            {
+                isValid = true;
+            }
+            else
+            {
+                isValid = false;
+                break;
+            }
+        }
+	}
+
+    if (isValid)
+    {
+        validUsenames.Add(inputArray[i]);
+    }
+}
+
+//OUTPUT
+foreach (var username in validUsenames)
+{
+    Console.WriteLine(username);
+}
