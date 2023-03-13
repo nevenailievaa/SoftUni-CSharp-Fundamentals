e37dd1940c34aab2b05204578a85e2682b007796
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-13 21:29:27 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/04.CaesarCipher.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+//INPUT
+char[] text = Console.ReadLine().ToCharArray();
+
+//ACTION
+List<char> encryptedText = new List<char>();
+
+for (int i = 0; i < text.Length; i++)
+{
+    int currEncryptedCharValue = text[i] + 3;
+    encryptedText.Add((char)currEncryptedCharValue);
+
+    //OUTPUT
+    Console.Write(encryptedText[i]);
+}
