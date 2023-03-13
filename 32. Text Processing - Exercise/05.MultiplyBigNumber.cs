9f681e151c89f23581a1c5b52e5e5e24cbb55dee
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-13 21:29:40 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/05.MultiplyBigNumber.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+//INPUT
+string bigNum = Console.ReadLine();
+int multiplier = int.Parse(Console.ReadLine());
+
+//ACTION
+if (multiplier == 0)
+{
+    Console.WriteLine(0);
+    return;
+}
+
+StringBuilder result = new StringBuilder();
+int overflowNum = 0;
+
+for (int i = bigNum.Length - 1; i >= 0; i--)
+{
+    int bigNumDigit = int.Parse(bigNum[i].ToString());
+
+    int product = bigNumDigit * multiplier + overflowNum;
+
+    int resultDigit = product % 10;
+
+    overflowNum = product / 10;
+    result.Insert(0, resultDigit);
+}
+
+if (overflowNum != 0)
+{
+    result.Insert(0, overflowNum);
+}
+
+Console.WriteLine(result);
