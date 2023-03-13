377f79ce3dd4bc44d7d96114f39873bd3e2cea69
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-13 13:00:21 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/02.CharacterMultiplier.cs
@@ -0,0 +1,62 @@
+﻿//INPUT
+string[] stringsInput = Console.ReadLine().Split();
+string stringOne = stringsInput[0];
+string stringTwo = stringsInput[1];
+
+//OUTPUT
+Console.WriteLine(CharacterMultiplier(stringOne, stringTwo));
+
+//ACTION
+static int CharacterMultiplier(string stringOne, string stringTwo)
+{
+    //Multiplication Length
+    int multiplicationLength = 0;
+    bool areEqual = false;
+
+    if (stringOne.Length >= stringTwo.Length)
+    {
+        multiplicationLength = stringTwo.Length;
+
+        if (stringOne.Length == stringTwo.Length)
+        {
+            areEqual = true;
+        }
+    }
+    else
+    {
+        multiplicationLength = stringOne.Length;
+    }
+
+    //Multiplicator
+    int sum = 0;
+    char[] stringOneCharArr = stringOne.ToCharArray();
+    char[] stringTwoCharArr = stringTwo.ToCharArray();
+
+    for (int i = 0; i < multiplicationLength; i++)
+    {
+        sum += stringOneCharArr[i] * stringTwoCharArr[i];
+    }
+
+    //Not Equal Case
+    if (!areEqual)
+    {
+        if (stringOne.Length > stringTwo.Length)
+        {
+            multiplicationLength = stringOne.Length - stringTwo.Length;
+            for (int i = stringOne.Length - multiplicationLength; i < stringOne.Length; i++)
+            {
+                sum += stringOneCharArr[i];
+            }
+        }
+        else
+        {
+            multiplicationLength = stringTwo.Length - stringOne.Length;
+            for (int i = stringTwo.Length - multiplicationLength; i < stringTwo.Length; i++)
+            {
+                sum += stringTwoCharArr[i];
+            }
+        }
+    }
+
+    return sum;
+}
