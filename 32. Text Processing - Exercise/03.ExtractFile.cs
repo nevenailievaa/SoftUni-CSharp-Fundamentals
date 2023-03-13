976ed09e2b6d8681d5073dc1a171cbeb0896ae69
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-13 21:29:16 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/03.ExtractFile.cs
@@ -0,0 +1,11 @@
+﻿//INPUT
+string[] fileLocation = Console.ReadLine().Split(@"\");
+
+//ACTION
+string[] nameAndExtension = fileLocation[fileLocation.Length -1].Split(".");
+string fileName = nameAndExtension[0];
+string fileExtension = nameAndExtension[1];
+
+//OUTPUT
+Console.WriteLine($"File name: {fileName}");
+Console.WriteLine($"File extension: {fileExtension}");
