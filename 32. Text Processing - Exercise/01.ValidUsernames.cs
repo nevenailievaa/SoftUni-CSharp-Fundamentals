0b18462d1b82b41ae35d8951b557772f4fa6adda
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-15 18:02:46 +0200

    Update 01.ValidUsernames.cs

--- 32. Text Processing - Exercise/01.ValidUsernames.cs
+++ 32. Text Processing - Exercise/01.ValidUsernames.cs
@@ -52,3 +52,9 @@ foreach (var username in validUsenames)
 {
     Console.WriteLine(username);
 }
+
+//Create a program that reads user names on a single line (joined by ", ")
+//and prints all valid usernames.
+//A valid username:
+//· has length between 3 and 16 characters
+//· contains only letters, numbers, hyphens and underscores
