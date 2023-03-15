fc4e0f52b061fe6ab550a198c6fb3cc9e95297f8
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-15 18:05:01 +0200

    Update 04.CaesarCipher.cs

--- 32. Text Processing - Exercise/04.CaesarCipher.cs
+++ 32. Text Processing - Exercise/04.CaesarCipher.cs
@@ -14,3 +14,8 @@ for (int i = 0; i < text.Length; i++)
     //OUTPUT
     Console.Write(encryptedText[i]);
 }
+
+//Create a program that returns an encrypted version of the same text.
+//Encrypt the text by shifting each character with three positions forward.
+//For example, A would be replaced by D, B would become E and so on.
+//Print the encrypted text.
