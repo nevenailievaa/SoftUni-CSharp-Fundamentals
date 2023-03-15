787da23449cda12ecde1b05f3e71d43d92bc842f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-15 18:03:17 +0200

    Update 02.CharacterMultiplier.cs

--- 32. Text Processing - Exercise/02.CharacterMultiplier.cs
+++ 32. Text Processing - Exercise/02.CharacterMultiplier.cs
@@ -60,3 +60,8 @@ static int CharacterMultiplier(string stringOne, string stringTwo)
 
     return sum;
 }
+
+//Create a method that takes two strings as arguments and returns the sum of their
+//character codes multiplied. Multiply str1[0] with str2[0] and add to the total sum.
+//Then continue with the next two characters. If one of the strings is longer than
+//the other, add the remaining character codes to the total sum without multiplication.
