36a7cd7a910e397a66d372adbcd771d7ff70fe2f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-15 15:59:49 +0200

    Add files via upload

---
+++ 32. Text Processing - Exercise/07.StringExplosion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+//INPUT
+StringBuilder currentString = new StringBuilder();
+currentString.Append(Console.ReadLine());
+
+//ACTION
+int power = 0;
+for (int i = 0; i < currentString.Length; i++)
+{
+	if (power == 0)
+	{
+		if (currentString[i] == '>')
+		{
+			power += int.Parse(currentString[i+1].ToString());
+		}
+	}
+	else
+	{
+		if (currentString[i] == '>')
+		{
+            power += int.Parse(currentString[i+1].ToString());
+        }
+		else
+		{
+			currentString.Remove(i, 1);
+			i--;
+            power--;
+        }
+	}
+}
+
+//OUTPUT
+Console.WriteLine(currentString);
