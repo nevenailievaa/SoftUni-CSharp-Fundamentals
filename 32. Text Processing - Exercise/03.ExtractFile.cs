8daa58deaba5c71a776f2929b4414c0d5ccafff0
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-15 18:04:21 +0200

    Update 03.ExtractFile.cs

--- 32. Text Processing - Exercise/03.ExtractFile.cs
+++ 32. Text Processing - Exercise/03.ExtractFile.cs
@@ -9,3 +9,6 @@ string fileExtension = nameAndExtension[1];
 //OUTPUT
 Console.WriteLine($"File name: {fileName}");
 Console.WriteLine($"File extension: {fileExtension}");
+
+//Create a program that reads the path to a file and subtracts
+//the file name and its extension.
