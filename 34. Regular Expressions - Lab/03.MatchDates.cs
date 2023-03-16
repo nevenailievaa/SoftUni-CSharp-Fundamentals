0b39e6d21e4078fb4cd2faa760e052b888359325
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 23:27:59 +0200

    Add files via upload

---
+++ 34. Regular Expressions - Lab/03.MatchDates.cs
@@ -0,0 +1,15 @@
+﻿//INPUT
+using System.Text.RegularExpressions;
+
+string dates = Console.ReadLine();
+
+//ACTION
+Regex dateRegex = new Regex(@"(?<day>\d{2})(\.|-|\/)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})");
+
+MatchCollection datesMatches = dateRegex.Matches(dates);
+
+//OUTPUT
+foreach (Match date in datesMatches)
+{
+    Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, Year: {date.Groups["year"]}");
+}
