b4abfd2b792eba79be58bce813ae96659e8bd5b0
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 21:08:42 +0200

    Update 01.MatchFullName.cs

--- 34. Regular Expressions - Lab/01.MatchFullName.cs
+++ 34. Regular Expressions - Lab/01.MatchFullName.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 //INPUT
 string names = Console.ReadLine();
@@ -10,3 +10,16 @@ MatchCollection matchCollection = nameRegex.Matches(names);
 
 //OUTPUT
 Console.WriteLine(String.Join(" ", matchCollection));
+
+//Write a C# Program to match full names from a list of names
+//and print them on the console.
+
+//First, create a regular expression to match a valid full name,
+//according to these conditions:
+
+//· A valid full name has the following characteristics:
+//· It consists of two words.
+//· Each word starts with a capital letter.
+//· After the first letter, it only contains lowercase letters afterward.
+//· Each of the two words should be at least two letters long.
+//· The two words are separated by a single space.
