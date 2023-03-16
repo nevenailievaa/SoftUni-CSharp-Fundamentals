faa0c6c2230f8a73472cdaa798a958b3be51dce9
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 23:29:59 +0200

    Update 02.MatchPhoneNumber.cs

--- 34. Regular Expressions - Lab/02.MatchPhoneNumber.cs
+++ 34. Regular Expressions - Lab/02.MatchPhoneNumber.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
 
 //INPUT
 string numbers = Console.ReadLine();
@@ -10,3 +11,18 @@ MatchCollection matchCollection = numbersRegex.Matches(numbers);
 
 //OUTPUT
 Console.WriteLine(String.Join(", ", matchCollection));
+
+
+//Create a regular expression to match a valid phone number from Sofia.
+//After you find all valid phones, print them on the console, separated
+//by a comma and a space ", ".
+
+//A valid number has the following characteristics:
+
+//· It starts with "+359"
+//· Then, it is followed by the area code (always 2)
+//· After that, it's followed by the number itself:
+//· The number consists of 7 digits (separated into
+//two groups of 3 and 4 digits respectively).
+//· The different parts are separated by either a
+//space or a hyphen ('-').
