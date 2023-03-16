b397b44270b5e85bfb7937a89d38a59d33b0e13d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 23:31:33 +0200

    Update 03.MatchDates.cs

--- 34. Regular Expressions - Lab/03.MatchDates.cs
+++ 34. Regular Expressions - Lab/03.MatchDates.cs
@@ -1,4 +1,6 @@
-﻿//INPUT
+//INPUT
+using System.Diagnostics.Metrics;
+using System.Runtime.Intrinsics.X86;
 using System.Text.RegularExpressions;
 
 string dates = Console.ReadLine();
@@ -13,3 +15,25 @@ foreach (Match date in datesMatches)
 {
     Console.WriteLine($"Day: {date.Groups["day"]}, Month: {date.Groups["month"]}, Year: {date.Groups["year"]}");
 }
+
+
+//Write a program, which matches a date in the format
+//"dd{separator}MMM{separator}yyyy". Use named capturing
+//groups in your regular expression.
+
+//Every valid date has the following characteristics:
+
+//· Always starts with two digits, followed by a separator.
+
+//· After that, it has one uppercase and two lowercase
+//letters (e.g. Jan, Mar).
+
+//· After that, it has a separator and exactly 4 digits
+//(for the year).
+
+//· The separator could be either of three things: a
+//period('. '), a hyphen('-') or a forward-slash ('/').
+
+//· The separator needs to be the same for the whole date
+//(e.g. 13.03.2016 is valid, 13.03/2016 is NOT). Use a group
+//backreference to check for this.
