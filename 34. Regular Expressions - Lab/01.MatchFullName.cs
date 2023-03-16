c61efd026cf911b26588d958357fc2e71907632b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 21:07:29 +0200

    Add files via upload

---
+++ 34. Regular Expressions - Lab/01.MatchFullName.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+//INPUT
+string names = Console.ReadLine();
+
+//ACTION
+Regex nameRegex = new Regex(@"\b[A-Z][a-z]+ [A-Z][a-z]+\b");
+
+MatchCollection matchCollection = nameRegex.Matches(names);
+
+//OUTPUT
+Console.WriteLine(String.Join(" ", matchCollection));
