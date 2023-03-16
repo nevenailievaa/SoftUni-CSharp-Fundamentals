431deb506ca6fa22bc18d960b693bb54b4b76deb
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-16 23:27:48 +0200

    Add files via upload

---
+++ 34. Regular Expressions - Lab/02.MatchPhoneNumber.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+//INPUT
+string numbers = Console.ReadLine();
+
+//ACTION
+Regex numbersRegex = new Regex(@"\+359( |-)[2]\1\d{3}\1\d{4}\b");
+
+MatchCollection matchCollection = numbersRegex.Matches(numbers);
+
+//OUTPUT
+Console.WriteLine(String.Join(", ", matchCollection));
