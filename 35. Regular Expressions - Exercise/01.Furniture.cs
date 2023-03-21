9790e4cf51305ba9bb3992bc81501ffed7fa2c6d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-21 14:05:18 +0200

    Add files via upload

---
+++ 35. Regular Expressions - Exercise/01.Furniture.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+//INPUT
+string input = String.Empty;
+
+//ACTION
+Regex regex = new Regex(@"\B>>(?<furniture>[A-z]+)<<(?<price>\d+(\.*\d*))!(?<quantity>\d+)");
+List<string> purchasedItems = new List<string>();
+double sum = 0;
+
+
+while ((input = Console.ReadLine()) != "Purchase")
+{
+    MatchCollection purchase = regex.Matches(input);
+
+    foreach (Match item in purchase)
+    {
+        string name = item.Groups["furniture"].Value;
+        double price = double.Parse(item.Groups["price"].Value);
+        int quantity = int.Parse(item.Groups["quantity"].Value);
+        purchasedItems.Add(name);
+        sum += price * quantity;
+    }
+}
+
+//OUTPUT
+Console.WriteLine("Bought furniture:");
+
+if (purchasedItems.Count > 0)
+{
+    Console.WriteLine(String.Join("\n", purchasedItems));
+}
+
+Console.WriteLine($"Total money spend: {sum:f2}");
