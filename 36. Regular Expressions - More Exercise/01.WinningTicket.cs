fe25da188b485e74c78fc74173775bcfeecb5441
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-22 14:29:20 +0200

    Add files via upload

---
+++ 36. Regular Expressions - More Exercise/01.WinningTicket.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+//INPUT
+string[] allTickets = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+//ACTION
+for (int i = 0; i < allTickets.Length; i++)
+{
+    string ticket = allTickets[i];
+
+    if (ticket.Length != 20)
+    {
+        Console.WriteLine("invalid ticket");
+        continue;
+    }
+
+    string leftSide = String.Join("", ticket.Take(10));
+    string rightSide = String.Join("", ticket.Skip(10));
+
+    string regex = @"([@#$^])\1{5,9}";
+    MatchCollection matchLeft = Regex.Matches(leftSide, regex);
+    MatchCollection matchRight = Regex.Matches(rightSide, regex);
+
+    if ((matchLeft.Count > 0 && matchRight.Count > 0) &&
+        (matchLeft[0].Value.Contains(matchRight[0].Value) ||
+        matchRight[0].Value.Contains(matchLeft[0].Value)))
+    {
+        char winningSymbol = matchLeft[0].Value[0];
+        int winningLength = Math.Min(matchLeft[0].Value.Length, matchRight[0].Value.Length);
+
+        string output = $"ticket \"{ticket}\" - {winningLength}{winningSymbol}";
+        if (winningLength == 10) output += " Jackpot!";
+        Console.WriteLine(output);
+    }
+    else
+    {
+        Console.WriteLine($"ticket \"{ticket}\" - no match");
+    }
+}
