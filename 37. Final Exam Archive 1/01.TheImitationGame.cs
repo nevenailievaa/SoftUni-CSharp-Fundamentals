7cbf3fa3204b9a2e5baf0ba4f67430d32f0c8f50
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-23 18:07:47 +0200

    Update 01.TheImitationGame.cs

--- 37. Final Exam Archive 1/01.TheImitationGame.cs
+++ 37. Final Exam Archive 1/01.TheImitationGame.cs
@@ -48,3 +48,20 @@ while ((command = Console.ReadLine()) != "Decode")
 
 //OUTPUT
 Console.WriteLine($"The decrypted message is: {decryptedMessage}");
+
+//During World War 2, you are a mathematician who has joined the cryptography team to
+//decipher the enemy's enigma code. Your job is to create a program to crack the codes. 
+
+//On the first line of the input, you will receive the encrypted message. After that,
+//until the "Decode" command is given, you will be receiving strings with instructions
+//for different operations that need to be performed upon the concealed message to interpret
+//it and reveal its true content. There are several types of instructions, split by '|'
+
+//"Move {number of letters}":
+//- Moves the first n letters to the back of the string
+
+//"Insert {index} {value}":
+//- Inserts the given value before the given index in the string
+
+//"ChangeAll {substring} {replacement}":
+//- Changes all occurrences of the given substring with the replacement text
