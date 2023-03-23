8ddbafa94b3abce6839d331d56fb760172479b32
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-23 18:06:32 +0200

    Add files via upload

---
+++ 37. Final Exam Archive 1/01.TheImitationGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+//INPUT
+string encryptedMessage = Console.ReadLine();
+StringBuilder decryptedMessage = new StringBuilder(encryptedMessage);
+
+//ACTION
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Decode")
+{
+    string[] commandArray = command.Split("|");
+
+    //Move Command
+    if (command.Contains("Move"))
+	{
+		int numberOfLettersToMove = int.Parse(commandArray[1]);
+        string lettersToMove = decryptedMessage.ToString().Substring(0, numberOfLettersToMove);
+
+        decryptedMessage.Remove(0, numberOfLettersToMove);
+        decryptedMessage.Append(lettersToMove);
+    }
+
+    //Insert Command
+	else if (command.Contains("Insert"))
+	{
+        int index = int.Parse(commandArray[1]);
+        string insertValue = commandArray[2];
+
+        decryptedMessage = decryptedMessage.Insert(index, insertValue);
+    }
+
+    //ChangeAll Command
+    else if (command.Contains("ChangeAll"))
+	{
+        char substringToChange = char.Parse(commandArray[1]);
+        char replacement = char.Parse(commandArray[2]);
+
+        for (int i = 0; i < decryptedMessage.Length; i++)
+        {
+            if (decryptedMessage[i] == substringToChange)
+            {
+                decryptedMessage[i] = replacement;
+            }
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"The decrypted message is: {decryptedMessage}");
