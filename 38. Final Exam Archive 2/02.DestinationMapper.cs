a7f33f56191ab0609f7288232336ce2d72d6082b
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-26 23:42:44 +0300

    Update 02.DestinationMapper.cs

--- 38. Final Exam Archive 2/02.DestinationMapper.cs
+++ 38. Final Exam Archive 2/02.DestinationMapper.cs
@@ -18,3 +18,23 @@ foreach (Match place in placesMatch)
 //OUTPUT
 Console.WriteLine($"Destinations: {String.Join(", ", placesList)}");
 Console.WriteLine($"Travel Points: {points}");
+
+//Now that you have planned out your tour, you are ready to go! Your next task is to
+//mark all the points on the map that you are going to visit.
+
+//You will be given a string representing some places on the map. You have to filter
+//only the valid ones. A valid location is:
+
+//Surrounded by "=" or "/" on both sides (the first and the last symbols must match)
+//After the first "=" or "/" there should be only letters (the first must be upper-case,
+//other letters could be upper or lower-case)
+//The letters must be at least 3
+//Example: In the string "=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=" only the
+//first two locations are valid.
+
+//After you have matched all the valid locations, you have to calculate travel points.
+//They are calculated by summing the lengths of all the valid destinations that you have
+//found on the map. 
+
+//In the end, on the first line, print: "Destinations: {destinations joined by ', '}". 
+//On the second line, print "Travel Points: {travel_points}".
