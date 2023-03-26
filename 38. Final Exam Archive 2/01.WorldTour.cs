b7bbe59e605c854deac47be50e195462734e7feb
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-26 23:41:25 +0300

    Update 01.WorldTour.cs

--- 38. Final Exam Archive 2/01.WorldTour.cs
+++ 38. Final Exam Archive 2/01.WorldTour.cs
@@ -53,4 +53,21 @@ while ((command = Console.ReadLine()) != "Travel")
 }
 
 //OUTPUT
-Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+//Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+
+//You are a world traveler, and your next goal is to make a world tour. To do that, you have to plan out
+//everything first. To start with, you would like to plan out all of your stops where you will have a break.
+//On the first line, you will be given a string containing all of your stops. Until you receive the command
+//"Travel", you will be given some commands to manipulate that initial string. The commands can be:
+
+//"Add Stop:{index}:{string}":
+//Insert the given string at that index only if the index is valid
+
+//"Remove Stop:{start_index}:{end_index}":
+//Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid
+
+//"Switch:{old_string}:{new_string}":
+//If the old string is in the initial string, replace it with the new one (all occurrences)
+
+//Note: After each command, print the current state of the string!
+//After the "Travel" command, print the following: "Ready for world tour! Planned stops: {string}"
