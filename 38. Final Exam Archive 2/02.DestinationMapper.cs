385eff7be759f119f47b46c0bcb713de598f05bd
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-26 23:40:07 +0300

    Add files via upload

---
+++ 38. Final Exam Archive 2/02.DestinationMapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+//INPUT
+string inputPlaces = Console.ReadLine();
+
+//ACTION
+Regex placeRegex = new Regex(@"(?<separator>=|\/)(?<destination>[A-Z][A-Za-z]{2,})\k<separator>");
+MatchCollection placesMatch = placeRegex.Matches(inputPlaces);
+List<string> placesList = new List<string>();
+
+int points = 0;
+foreach (Match place in placesMatch)
+{
+    points += place.Groups["destination"].Value.Length;
+    placesList.Add(place.Groups["destination"].Value);
+}
+
+//OUTPUT
+Console.WriteLine($"Destinations: {String.Join(", ", placesList)}");
+Console.WriteLine($"Travel Points: {points}");
