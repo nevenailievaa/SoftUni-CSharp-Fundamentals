e7bcecf02d1c430e7afe2fb40629b807d437f46f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-27 13:52:21 +0300

    Update 03.PlantDiscovery-Version2.cs

--- 38. Final Exam Archive 2/03.PlantDiscovery-Version2.cs
+++ 38. Final Exam Archive 2/03.PlantDiscovery-Version2.cs
@@ -97,3 +97,31 @@ foreach (var currentPlant in plantAndRarity)
         Console.WriteLine($"- {currentPlant.Key}; Rarity: {currentPlant.Value}; Rating: {averageRate:f2}");
     }
 }
+
+//You have now returned from your world tour. On your way, you have discovered some
+//new plants, and you want to gather some information about them and create an exhibition
+//to see which plant is highest rated.
+
+//On the first line, you will receive a number n. On the next n lines, you will be
+//given some information about the plants that you have discovered in the format:
+//"{plant}<->{rarity}".Store that information because you will need it later. If
+//you receive a plant more than once, update its rarity.
+
+//After that, until you receive the command "Exhibition", you will be given some of
+//these commands:
+
+//"Rate: {plant} - {rating}" – add the given rating to the plant (store all ratings)
+//"Update: {plant} - {new_rarity}" – update the rarity of the plant with the new one
+//"Reset: {plant}" – remove all the ratings of the given plant
+
+//Note: If any given plant name is invalid, print "error"
+
+//After the command "Exhibition", print the information that you have about the plants
+//in the following format:
+
+//"Plants for the exhibition:
+//- { plant_name1}; Rarity: { rarity}; Rating: { average_rating}
+//- { plant_name2}; Rarity: { rarity}; Rating: { average_rating}
+//- { plant_nameN}; Rarity: { rarity}; Rating: { average_rating}
+
+//The average rating should be formatted to the second decimal place.
