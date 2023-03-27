40a6fbf99e3f81105b0a2b3b1619cb5618dd5d2f
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-27 14:08:31 +0300

    Add files via upload

---
+++ 06. Data Types and Variables - More Exercise/06.BalancedBrackets.cs
@@ -0,0 +1,40 @@
+﻿//INPUT
+int n = int.Parse(Console.ReadLine());
+
+//ACTION
+bool isBalanced = true;
+bool openedBracket = false;
+
+for (int line = 0; line < n; line++)
+{
+    string input = Console.ReadLine();
+    if (input == "(")
+    {
+        if (openedBracket)
+        {
+            isBalanced = false;
+        }
+        else
+        {
+            openedBracket = true;
+        }
+    }
+    else if (input == ")")
+    {
+        if (openedBracket)
+        {
+            openedBracket = false;
+        }
+        else
+        {
+            isBalanced = false;
+        }
+    }
+}
+
+//OUTPUT
+if (openedBracket)
+{
+    isBalanced = false;
+}
+Console.WriteLine(isBalanced ? "BALANCED" : "UNBALANCED");
