8d8ba4fa898718cd96aa854d9c6a30b60f502ec3
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-27 14:13:48 +0300

    Add files via upload

---
+++ 09. Arrays - More Exercise/02.PascalTriangle.cs
@@ -0,0 +1,23 @@
+﻿//INPUT
+int num = int.Parse(Console.ReadLine());
+
+//ACTION
+int[] newArray = new int[num + 1];
+int[] oldArray = new int[num + 1];
+newArray[1] = 1;
+Console.WriteLine(newArray[1]);
+
+for (int row = 1; row < num; row++)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        oldArray[i] = newArray[i];
+    }
+
+    for (int i = 1; i <= row + 1; i++)
+    {
+        newArray[i] = oldArray[i - 1] + oldArray[i];
+        Console.Write(newArray[i] + " ");
+    }
+    Console.WriteLine();
+}
