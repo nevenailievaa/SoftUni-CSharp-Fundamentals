fa7039842d80176fb286138b4766c793c6348b38
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-27 14:15:52 +0300

    Add files via upload

---
+++ 09. Arrays - More Exercise/03.RecursiveFibonacci.cs
@@ -0,0 +1,16 @@
+﻿//INPUT
+int input = int.Parse(Console.ReadLine());
+
+//OUTPUT
+Console.WriteLine(GetFibonacci(input));
+
+static int GetFibonacci(int n)
+{
+    if (n == 2 || n == 1)
+    {
+        return 1;
+    }
+
+    int number = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+    return number;
+}
