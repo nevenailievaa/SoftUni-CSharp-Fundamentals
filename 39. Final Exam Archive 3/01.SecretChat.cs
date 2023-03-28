7835d26f25cc67444dda480e080fc361826c8458
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-28 17:41:15 +0300

    Update 01.SecretChat.cs

--- 39. Final Exam Archive 3/01.SecretChat.cs
+++ 39. Final Exam Archive 3/01.SecretChat.cs
@@ -65,3 +65,25 @@ while ((command = Console.ReadLine()) != "Reveal")
 
 //OUTPUT
 Console.WriteLine($"You have a new text message: {message}");
+
+//You have plenty of free time, so you decide to write a program that
+//conceals and reveals your received messages. Go ahead and type it in!
+
+//On the first line of the input, you will receive the concealed message.
+//After that, until the "Reveal" command is given, you will receive strings
+//with instructions for different operations that need to be performed upon
+//the concealed message to interpret it and reveal its actual content. There
+//are several types of instructions, split by ":|:"
+
+// - "InsertSpace:|:{index}":
+//Inserts a single space at the given index. The given index will always be valid.
+
+// - "Reverse:|:{substring}":
+//If the message contains the given substring, cut it out, reverse it and add it
+//at the end of the message. 
+//If not, print "error".
+//This operation should replace only the first occurrence of the given substring if
+//there are two or more occurrences.
+
+// - "ChangeAll:|:{substring}:|:{replacement}":
+//Changes all occurrences of the given substring with the replacement text.
