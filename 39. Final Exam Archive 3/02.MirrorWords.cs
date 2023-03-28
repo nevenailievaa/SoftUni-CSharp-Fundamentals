60edf709539149e758671e30e3c256b33ad31268
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-28 17:37:02 +0300

    Add files via upload

---
+++ 39. Final Exam Archive 3/02.MirrorWords.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//INPUT
+string input = Console.ReadLine();
+
+//ACTION
+Regex pairsRegex = new Regex(@"(?<Separator>#|@)(?<Word>[A-Za-z]{3,})\k<Separator>{2}(?<WordTwo>[A-Za-z]{3,})");
+MatchCollection matches = pairsRegex.Matches(input);
+
+Dictionary<string, string> mirroredWords = new Dictionary<string, string>();
+
+foreach (Match match in matches)
+{
+    char[] wordTwoArray = (match.Groups["WordTwo"].Value).ToCharArray();
+    Array.Reverse(wordTwoArray);
+    string wordTwoReversed = new string(wordTwoArray);
+
+    if (match.Groups["Word"].Value == wordTwoReversed)
+    {
+        string wordOne = match.Groups["Word"].Value;
+        string wordTwo = match.Groups["WordTwo"].Value;
+        mirroredWords.Add(wordOne, wordTwo);
+    }
+}
+
+//OUTPUT
+List<StringBuilder> outputWords = new List<StringBuilder>();
+
+if (matches.Count == 0)
+{
+    Console.WriteLine("No word pairs found!");
+}
+else
+{
+    Console.WriteLine($"{matches.Count} word pairs found!");
+}
+
+if (mirroredWords.Count > 0)
+{
+    Console.WriteLine("The mirror words are:");
+    foreach (var word in mirroredWords)
+    {
+        StringBuilder currentMirroredWords = new StringBuilder();
+        currentMirroredWords.Append(word.Key);
+        currentMirroredWords.Append(" <=> ");
+        currentMirroredWords.Append(word.Value);
+
+        outputWords.Add(currentMirroredWords);
+    }
+    Console.WriteLine(string.Join(", ", outputWords));
+}
+else
+{
+    Console.WriteLine("No mirror words!");
+}
