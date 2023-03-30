2c4ce677bb7f837fc1cd8acb0e7e058a7c31d6a6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-30 21:10:08 +0300

    Add files via upload

---
+++ 15. List - Exercise/09.PokemonDontGo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class PokemonDontGo
+{
+    static void ListModify(List<int> list, int currentOut)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i] <= currentOut)
+            {
+                list[i] += currentOut;
+            }
+            else
+            {
+                list[i] -= currentOut;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        //INPUT
+        List<int> list = Console.ReadLine()
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        //ACTION
+        int sum = 0;
+        int currentOut = 0;
+
+        while (list.Count > 0)
+        {
+            int index = int.Parse(Console.ReadLine());
+            if (index < 0)
+            {
+                currentOut = list[0];
+                list[0] = list[list.Count - 1];
+            }
+            else if (index > list.Count - 1)
+            {
+                currentOut = list[list.Count - 1];
+                list[list.Count - 1] = list[0];
+            }
+            else
+            {
+                currentOut = list[index];
+                list.RemoveAt(index);
+            }
+
+            ListModify(list, currentOut);
+            sum += currentOut;
+
+        }
+
+        //OUTPUT
+        Console.WriteLine(sum);
+    }
+}
