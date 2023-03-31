3c69dbf67de73538e028c16d9a582defcafa226d
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-31 22:52:10 +0300

    Update 01.PasswordReset.cs

--- 40. Final Exam Archive 4/01.PasswordReset.cs
+++ 40. Final Exam Archive 4/01.PasswordReset.cs
@@ -58,3 +58,25 @@ while ((command = Console.ReadLine()) != "Done")
 
 //OUTPUT
 Console.WriteLine($"Your password is: {password}");
+
+//Yet again, you have forgotten your password. Naturally, it's not the first time
+//this has happened. Actually, you got so tired of it that you decided to help yourself
+//with an intelligent solution.
+
+//Write a password reset program that performs a series of commands upon a predefined string.
+//First, you will receive a string, and afterward, until the command "Done" is given, you will
+//be receiving strings with commands split by a single space. The commands will be the following:
+
+//- "TakeOdd"
+// Takes only the characters at odd indices and concatenates them to obtain the new raw password
+//and then prints it.
+
+//- "Cut {index} {length}"
+//Gets the substring with the given length starting from the given index from the password and
+//removes its first occurrence, then prints the password on the console.
+//The given index and the length will always be valid.
+
+//- "Substitute {substring} {substitute}"
+//If the raw password contains the given substring, replaces all of its occurrences with the
+//substitute text given and prints the result.
+//If it doesn't, prints "Nothing to replace!".
