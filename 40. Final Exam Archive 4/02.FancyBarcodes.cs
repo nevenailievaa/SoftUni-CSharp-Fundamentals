39926d1ceb9aea0628888aea99edc586d0cb4aa4
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-31 22:47:58 +0300

    Add files via upload

---
+++ 40. Final Exam Archive 4/02.FancyBarcodes.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+//INPUT
+int barcodesCount = int.Parse(Console.ReadLine());
+
+//ACTION
+Regex barcodeRegex = new Regex(@"^(?<startSymbols>@#+)(?<barcode>[A-Z][A-Za-z0-9]{4,}[A-Z])(?<endSymbols>@#+)$");
+
+for (int i = 0; i < barcodesCount; i++)
+{
+    string currentBarcode = Console.ReadLine();
+
+	if (barcodeRegex.IsMatch(currentBarcode))
+	{
+		StringBuilder currentGroup = new StringBuilder("00");
+		if (currentBarcode.Any(char.IsDigit))
+		{
+			currentGroup.Clear();
+			for (int j = 0; j < currentBarcode.Length; j++)
+			{
+				if (char.IsDigit(currentBarcode[j]))
+				{
+					currentGroup.Append(currentBarcode[j]);
+                }
+			}
+		}
+		Console.WriteLine($"Product group: {currentGroup}");
+    }
+	else
+	{
+		Console.WriteLine("Invalid barcode");
+	}
+}
