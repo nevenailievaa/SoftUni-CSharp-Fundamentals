8633f5e190c3f7a2f5925a1d25ed069b12ca2a5e
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-31 22:47:46 +0300

    Add files via upload

---
+++ 40. Final Exam Archive 4/01.PasswordReset.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+//INPUT
+StringBuilder password = new StringBuilder(Console.ReadLine());
+
+//ACTION
+string command = string.Empty;
+
+while ((command = Console.ReadLine()) != "Done")
+{
+    string[] commandArray = command.Split();
+    string commandType = commandArray[0];
+
+    StringBuilder newRawPassword = new StringBuilder();
+
+    //TakeOdd Command
+    if (commandType == "TakeOdd")
+    {
+        for (int i = 0; i < password.Length; i++)
+        {
+            if (i % 2 == 1)
+            {
+                newRawPassword.Append(password[i]);
+            }
+        }
+        password.Clear();
+        password = newRawPassword;
+        Console.WriteLine(newRawPassword);
+    }
+
+    //Cut Command
+    else if (commandType == "Cut")
+    {
+        int index = int.Parse(commandArray[1]);
+        int length = int.Parse(commandArray[2]);
+
+        password.Remove(index, length);
+        Console.WriteLine(password);
+    }
+
+    //Substitute Command
+    else if (commandType == "Substitute")
+    {
+        string substring = commandArray[1];
+        string substitute = commandArray[2];
+
+        if (password.ToString().Contains(substring))
+        {
+            password.Replace(substring, substitute);
+            Console.WriteLine(password);
+        }
+        else
+        {
+            Console.WriteLine("Nothing to replace!");
+        }
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"Your password is: {password}");
