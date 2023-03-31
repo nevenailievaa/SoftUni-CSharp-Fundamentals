bfaf5f464eb85e648a118908117d1c23595c5701
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-31 22:56:37 +0300

    Update 03.HeroesOfCodeAndLogicVII-Version1.cs

--- 40. Final Exam Archive 4/03.HeroesOfCodeAndLogicVII-Version1.cs
+++ 40. Final Exam Archive 4/03.HeroesOfCodeAndLogicVII-Version1.cs
@@ -116,3 +116,51 @@ foreach (var hero in heroesAndHitpoints)
     Console.WriteLine($"HP: {hero.Value}");
     Console.WriteLine($"MP: {heroesAndManapoints[hero.Key]}");
 }
+
+//You got your hands on the most recent update on the best MMORPG of all time –
+//Heroes of Code and Logic. You want to play it all day long! So cancel all other
+//arrangements and create your party!
+//On the first line of the standard input, you will receive an integer n – the
+//number of heroes that you can choose for your party. On the next n lines, the
+//heroes themselves will follow with their hit points and mana points separated
+//by a single space in the following format: 
+//"{hero name} {HP} {MP}"
+
+//HP stands for hit points and MP for mana points
+//a hero can have a maximum of 100 HP and 200 MP
+//After you have successfully picked your heroes, you can start playing the game.
+//You will be receiving different commands, each on a new line, separated by " – ",
+//until the "End" command is given. 
+//There are several actions that the heroes can perform:
+
+//"CastSpell – {hero name} – {MP needed} – {spell name}"
+//If the hero has the required MP, he casts the spell, thus reducing his MP. Print
+//this message: 
+//"{hero name} has successfully cast {spell name} and now has {mana points left} MP!"
+//If the hero is unable to cast the spell print:
+//"{hero name} does not have enough MP to cast {spell name}!"
+
+//"TakeDamage – {hero name} – {damage} – {attacker}"
+//Reduce the hero HP by the given damage amount. If the hero is still alive (his HP is
+//greater than 0) print:
+//"{hero name} was hit for {damage} HP by {attacker} and now has {current HP} HP left!"
+//If the hero has died, remove him from your party and print:
+//"{hero name} has been killed by {attacker}!"
+
+//"Recharge – {hero name} – {amount}"
+//The hero increases his MP. If it brings the MP of the hero above the maximum value (200),
+//MP is increased to 200. (the MP can't go over the maximum value).
+//Print the following message:
+//"{hero name} recharged for {amount recovered} MP!"
+
+//"Heal – {hero name} – {amount}"
+//The hero increases his HP. If a command is given that would bring the HP of the hero above
+//the maximum value (100), HP is increased to 100 (the HP can't go over the maximum value).
+//Print the following message:
+//"{hero name} healed for {amount recovered} HP!"
+
+//Print all members of your party who are still alive, in the following format (their HP/MP
+//need to be indented 2 spaces):
+//"{hero name}
+//HP: {current HP}
+//MP: {current MP}"
