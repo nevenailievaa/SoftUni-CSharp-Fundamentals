6046097ee70c037ec531b083ca7c9c35d7fc7ce6
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-03-31 22:54:13 +0300

    Update 02.FancyBarcodes.cs

--- 40. Final Exam Archive 4/02.FancyBarcodes.cs
+++ 40. Final Exam Archive 4/02.FancyBarcodes.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Text.RegularExpressions;
 
 //INPUT
@@ -22,13 +22,34 @@ for (int i = 0; i < barcodesCount; i++)
 				if (char.IsDigit(currentBarcode[j]))
 				{
 					currentGroup.Append(currentBarcode[j]);
-                }
+                                }
 			}
 		}
 		Console.WriteLine($"Product group: {currentGroup}");
-    }
+        }
 	else
 	{
 		Console.WriteLine("Invalid barcode");
 	}
 }
+
+//Your first task is to determine if the given sequence of characters is a valid barcode or not. 
+
+//Each line must not contain anything else but a valid barcode. A barcode is valid when:
+// - It is surrounded by a "@" followed by one or more "#"
+// - It is at least 6 characters long (without the surrounding "@" or "#")
+// - It starts with a capital letter
+// - It contains only letters (lower and upper case) and digits
+// - It ends with a capital letter
+
+//Examples of valid barcodes: @###Che46sE@##, @#FreshFisH@#, @###Brea0D@###, @##Che46sE@##
+//Examples of invalid barcodes: ##InvaliDiteM##, @InvalidIteM@, @#Invalid_IteM@#
+
+//Next, you have to determine the product group of the item from the barcode. The product group is
+//obtained by concatenating all the digits found in the barcode. If there are no digits present in
+//the barcode, the default product group is "00".
+
+//Examples:  
+//@#FreshFisH@# -> product group: 00
+//@###Brea0D@### -> product group: 0
+//@##Che4s6E@## -> product group: 46
