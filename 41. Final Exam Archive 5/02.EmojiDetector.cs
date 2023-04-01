78e46a74d3cb6bbbfce07a02e5c3a59b9ba615b3
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-04-01 12:39:35 +0300

    Update 02.EmojiDetector.cs

--- 41. Final Exam Archive 5/02.EmojiDetector.cs
+++ 41. Final Exam Archive 5/02.EmojiDetector.cs
@@ -41,3 +41,26 @@ foreach (var emoji in emojisMatches)
 Console.WriteLine($"Cool threshold: {coolThreshold}");
 Console.WriteLine($"{emojisMatches.Count} emojis found in the text. The cool ones are:");
 Console.WriteLine(String.Join("\n", validEmojis));
+
+//Your task is to write a program that extracts emojis from a text and find
+//the threshold based on the input.
+//You have to get your cool threshold. It is obtained by multiplying all the
+//digits found in the input.  The cool threshold could be a huge number, so
+//be mindful.
+
+//An emoji is valid when:
+//- It is surrounded by 2 characters, either "::" or "**"
+//- It is at least 3 characters long (without the surrounding symbols)
+//- It starts with a capital letter
+//- Continues with lowercase letters only
+
+//Examples of valid emojis: ::Joy::, **Banana**, ::Wink::
+//Examples of invalid emojis: ::Joy**, ::fox:es:, **Monk3ys**, :Snak::Es::
+
+//You need to count all valid emojis in the text and calculate their coolness.
+//The coolness of the emoji is determined by summing all the ASCII values of
+//all letters in the emoji. 
+//Examples: ::Joy:: - 306, **Banana** - 577, ::Wink:: - 409
+//You need to print the result of the cool threshold and, after that to take
+//all emojis out of the text, count them and print only the cool ones on the
+//console.
