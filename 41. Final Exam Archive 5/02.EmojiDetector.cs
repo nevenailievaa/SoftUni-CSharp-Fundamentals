fd5b400ba696f3ca1e448fe5c45b847ea2eb5717
Nevena Firkóva <118193964+nevenafirkova@users.noreply.github.com>
2023-04-01 11:47:43 +0300

    Add files via upload

---
+++ 41. Final Exam Archive 5/02.EmojiDetector.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+//INPUT
+string inputText = Console.ReadLine();
+
+//Regexes
+Regex numbersRegex = new Regex(@"(?<numbers>[0-9])");
+Regex emojisRegex = new Regex(@"(?<separator>\:{2}|\*{2})(?<emoji>[A-Z][a-z]{2,})\k<separator>");
+
+//ACTION
+//Numbers
+ulong coolThreshold = 1U;
+MatchCollection numbersMatches = numbersRegex.Matches(inputText);
+
+foreach (Match match in numbersMatches)
+{
+    coolThreshold *= ulong.Parse(match.Value);
+}
+
+//Emojis
+MatchCollection emojisMatches = emojisRegex.Matches(inputText);
+List<string> validEmojis = new List<string>();
+
+foreach (var emoji in emojisMatches)
+{
+    string currentEmoji = emoji.ToString();
+    ulong currentCoolness = 0U;
+
+    for (int i = 2; i < currentEmoji.Length-2; i++)
+    {
+        currentCoolness += currentEmoji[i];
+    }
+    if (currentCoolness >= coolThreshold)
+    {
+        validEmojis.Add(currentEmoji);
+    }
+}
+
+//OUTPUT
+Console.WriteLine($"Cool threshold: {coolThreshold}");
+Console.WriteLine($"{emojisMatches.Count} emojis found in the text. The cool ones are:");
+Console.WriteLine(String.Join("\n", validEmojis));
